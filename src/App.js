779026cca3966f05d0f1073f27b8d6ab4ca6d46b
vaichu <vaishnavvaichu123@gmail.com>
2021-12-08 02:10:47 +0530

    Testing new component

--- src/App.js
+++ src/App.js
@@ -1,16 +1,20 @@
 import React from 'react';
 import './normalize.css';
-import './base.css';
+import './base.scss';
 import './App.css';
 import Header from './components/Header';
 import Footer from './components/Footer'
 import Body from './components/Body';
-
+import HomeBanner from './components/HomeBanner';
+import MinimalPropertyList from './components/MinimalPropertyList';
 function App() {
   return (
     <div className="VP">
       <Header />
-      <div className="body"></div>
+      <Body>
+        <HomeBanner />
+        <MinimalPropertyList />
+      </Body>
       <Footer />
     </div>
   );
