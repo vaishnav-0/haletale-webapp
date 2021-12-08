8005361d5bc3043525003e95d044b4d9cce76933
vaichu <vaishnavvaichu123@gmail.com>
2021-12-08 17:41:20 +0530

    .

--- src/components/MinimalPropertyList.js
+++ src/components/MinimalPropertyList.js
@@ -20,17 +20,24 @@ export default function () {
     }
     const listItems = [1, 2, 3, 4, 5, 6, 7, 8, 9].map((number) => <PropertyCard key={number.toString()} />);
     return (
-        <div>
-            <div ref={listRef} className="minimal-list-container">
-                {listItems}
+        <div className="property-list">
+            <div className="list-heading-container">
+                <div className="list-heading">Popular properties</div>
+                <button class="list-more-btn">SEE ALL</button>
             </div>
-            <button onClick={scrollLeftList}>
-                left
-            </button>
-            <button onClick={scrollRightList}>
-                right
-            </button>
+            <div className="minimal-list-container-wrapper">
+                <div ref={listRef} className="minimal-list-container">
+                    {listItems}
+                    <button className="left-scroll-btn" onClick={scrollLeftList}>
+                        &lt;
+                    </button>
+                    <button className="right-scroll-btn" onClick={scrollRightList}>
+                        &gt;
+                    </button>
+                </div>
+            </div>
         </div>
+
 
 
     );
