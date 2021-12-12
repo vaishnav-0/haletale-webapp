e10dd4bc51eb350a612c96f03cac82dd656f7ce9
vaichu <vaishnavvaichu123@gmail.com>
2021-12-12 11:33:30 +0530

    fav icon and image scroll

--- src/components/PropertyCardDetailed.js
+++ src/components/PropertyCardDetailed.js
@@ -1,17 +1,29 @@
 import React from 'react';
 import style from "./PropertyCardDetailed.module.scss";
-import propertyImg from "../assets/images/property.jpeg";
+import propertyImg1 from "../assets/images/16_9_house.png";
+import propertyImg2 from "../assets/images/housewide.jpg";
 import bedIcon from "../assets/icons/pro-details-icon1.png";
 import bathIcon from "../assets/icons/pro-details-icon2.png";
 import petIcon from "../assets/icons/pro-details-icon3.png";
 import sqftIcon from "../assets/icons/pro-details-icon4.png";
 import heartIcon from "../assets/icons/heart-thin.svg";
 export default function () {
+    const [fav, setFav] = React.useState(false);
     return (
         <div className={style["property-card"]}>
             <div className={style["property-card-container"]}>
                 <div className={style["image-slider"]}>
-                    <img src={propertyImg} />
+                    <div className={style["image-slider-container"]}>
+                        <div>
+                            <img src={propertyImg1} />
+
+                        </div>
+                        <div>
+                            <img src={propertyImg2} />
+
+                        </div>
+                    </div>
+
                 </div>
                 <div className={style["property-details"]}>
                     <div className={style["top-container"]}>
@@ -21,7 +33,7 @@ export default function () {
                             <div>130 Clinton Street - Toronto, ON</div>
                         </div>
                         <div className={style["fav-icon"]}>
-                            <img src={heartIcon} />
+                            <i onClick={() => setFav(!fav)} className={`${fav ? style.filled + " fas" : style.regular + " far"} far fa-heart`}></i>
                         </div>
                     </div>
                     <div className={style["bottom-container"]}>
