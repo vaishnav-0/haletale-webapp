471a02a2df7b372ab9f93ac3ed61f9aa1c1825a8
vaichu <vaishnavvaichu123@gmail.com>
2021-12-12 11:28:25 +0530

    added navigation

--- src/components/Searchbar.js
+++ src/components/Searchbar.js
@@ -1,15 +1,19 @@
 import React from 'react';
+import { useNavigate } from "react-router-dom";
 import "./Searchbar.scss";
 import searchIcon from "../assets/icons/search.png";
-export default function(){
-    return(
+export default function () {
+    let navigate = useNavigate();
+    return (
         <div className="searchbox-container">
             <div className="searchbox-input-field">
-                <input placeholder="Search Property, Neighbourhood or Address" type="text"/>
+                <input placeholder="Search Property, Neighbourhood or Address" type="text" />
             </div>
             <div className="searchbox-btn">
-                <button>
-                    <img src={searchIcon}/>
+                <button onClick={() => {
+                    navigate("/properties");
+                }}>
+                    <img src={searchIcon} />
                 </button>
             </div>
         </div>
