9021262754ad82577fe091fc5ba5958040561fc0
vaichu <vaishnavvaichu123@gmail.com>
2021-12-12 11:35:46 +0530

    filter modal and minor layout change

--- src/pages/PropertySearchListing.js
+++ src/pages/PropertySearchListing.js
@@ -1,22 +1,37 @@
 import React from 'react';
 import PropertyCardDetailed from '../components/PropertyCardDetailed';
+import FilterModel from '../components/FilterModal';
 import style from './PropertySearchListing.module.scss';
 import Layout from './Layout';
 
 export default function () {
+    const [filterOpen, setFilterOpen] = React.useState(false);
     return (
         <Layout>
+            {filterOpen &&
+                <div className={style["filter-background"]} />
+            }
             <div className={style["header"]}>
                 <div className={style["txt-container"]}>
                     <div>Toronto</div>
                     <div> 153 properties</div>
                 </div>
                 <div className={style["btn-container"]}>
-                    <button>F</button>
-                    <button>S</button>
+                    <button onClick={() => { setFilterOpen(true); }}><i className="fas fa-sliders-h"></i></button>
+                    <button><i className="fas fa-sort"></i></button>
                 </div>
+                {filterOpen &&
+                    <div className={style["filter-container"]}>
+                        <FilterModel onClose={()=>{
+                            setFilterOpen(false);
+                        }}/>
+
+                    </div>
+                }
+
             </div>
             <div className={style["search-list"]}>
+                <PropertyCardDetailed />
                 <PropertyCardDetailed />
                 <PropertyCardDetailed />
                 <PropertyCardDetailed />
