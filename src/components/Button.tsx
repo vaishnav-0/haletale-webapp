674fde9564c06d99dc697f708e47ab9f765bb4fc
vaichu <vaishnavvaichu123@gmail.com>
2021-12-26 23:36:24 +0530

    added prop

--- src/components/Button.tsx
+++ src/components/Button.tsx
@@ -1,13 +1,14 @@
 import React from 'react';
 import "./Button.scss";
-type props ={
+type props = {
     label: string;
+    onClick?: () => void;
 }
-export function ButtonHollow({ label }: props): JSX.Element {
-    return <button className="btn-smoothround-hollow">{label}</button>
+export function ButtonHollow({ label, onClick = () => { } }: props): JSX.Element {
+    return <button onClick={onClick} className="btn-smoothround-hollow">{label}</button>
 
 }
-export function ButtonSolid({ label }: props): JSX.Element {
-    return <button className="btn-smoothround-solid">{label}</button>
+export function ButtonSolid({ label , onClick = () => { } }: props): JSX.Element {
+    return <button onClick={onClick} className="btn-smoothround-solid">{label}</button>
 
 }
