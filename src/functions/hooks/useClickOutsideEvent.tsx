6f99411ec3c3cf092358d87c341ae79b42c3d94b
vaichu <vaishnavvaichu123@gmail.com>
2021-12-27 00:45:25 +0530

    removed type parameter

--- src/functions/hooks/useClickOutsideEvent.tsx
+++ src/functions/hooks/useClickOutsideEvent.tsx
@@ -1,20 +1,32 @@
 import React from "react";
+type ret = {
+    setExcludedFromClick: (elements: HTMLElement[]) => void
+}
 
-export function useClickOutsideEvent<T extends Node>(ref: { current: HTMLElement }, onClick: () => void = () => { }, exclude?: React.MutableRefObject<T>[]): void {
+export function useClickOutsideEvent(
+    ref: { current: HTMLElement },
+    onClick: () => void = () => { },
+    exclude?: HTMLElement[])
+    : ret {
+    const excluded = React.useRef<HTMLElement[]>([]);
+    if (exclude)
+        excluded.current = exclude;
     const handleClickOutside = (event: Event) => {
-        if (exclude)
-            for (let i = 0; i < exclude.length; i++) {
-                if (exclude[i].current.contains(event.target as Node))
-                    return;
-            }
+        for (let i = 0; i < excluded.current.length; i++) {
+            if (excluded.current[i]?.contains(event.target as Node))
+                return;
+        }
         if (ref.current && !ref.current.contains(event.target as Node)) {
             onClick();
         }
-    };
+
+    }
     React.useEffect(() => {
         document.addEventListener('click', handleClickOutside, true);
         return () => {
             document.removeEventListener('click', handleClickOutside, true);
         };
     }, []);
+    return { setExcludedFromClick: (elements) => { excluded.current = elements } };
+
 }
