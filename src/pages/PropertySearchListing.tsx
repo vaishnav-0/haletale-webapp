ffdb113cc1bc537e64b1897b6ae2052382587aaa
vaichu <vaishnavvaichu123@gmail.com>
2021-12-26 23:33:05 +0530

    added sort menu and restructure to avoid rerender of modal

--- src/pages/PropertySearchListing.tsx
+++ src/pages/PropertySearchListing.tsx
@@ -3,9 +3,15 @@ import PropertyCardDetailed from '../components/PropertyCardDetailed';
 import FilterModel from '../components/FilterModal';
 import style from './PropertySearchListing.module.scss';
 import Layout from './Layout';
+import RadioButton from '../components/Form/components/Radiobutton';
+import { useClickOutsideEvent } from '../functions/useClickOutsideEvent';
 
 export default function (): JSX.Element {
     const [filterOpen, setFilterOpen] = React.useState(false);
+    const [sortOpen, setSortOpen] = React.useState(false);
+    const sortRef = React.useRef<HTMLDivElement>(null!);
+    const sortButtonRef = React.useRef<HTMLButtonElement>(null!);
+    useClickOutsideEvent<HTMLButtonElement>(sortRef, () => setSortOpen(false), [sortButtonRef]);
     return (
         <Layout>
             {filterOpen &&
@@ -18,20 +24,24 @@ export default function (): JSX.Element {
                 </div>
                 <div className={style["btn-container"]}>
                     <button onClick={() => { setFilterOpen(true); }}><i className="fas fa-sliders-h"></i></button>
-                    <button><i className="fas fa-sort"></i></button>
+                    <button ref={sortButtonRef} onClick={() => { setSortOpen(!sortOpen) }}>
+                        <i className="fas fa-sort"></i>
+
+                    </button>
                 </div>
-                {filterOpen &&
-                    <div className={style["filter-wrapper"]}>
-
-                        <div className={style["filter-container"]}>
-                            <FilterModel onClose={() => {
-                                setFilterOpen(false);
-                            }} />
-
-                        </div>
+                    <div ref={sortRef} className={style["sort-popup"]} style={{ display: !sortOpen ? "none" : "revert" }}>
+                        <RadioButton name="sort" values={["Default", "Recent"]} />
                     </div>
+                <div className={style["filter-wrapper"]} style={{ display: !filterOpen ? "none" : "revert" }}>
 
-                }
+                    <div className={style["filter-container"]}>
+                        <FilterModel onClose={() => {
+                            setFilterOpen(false);
+                        }} />
+
+                    </div>
+                </div>
+
 
             </div>
             <div className={style["search-list"]}>
