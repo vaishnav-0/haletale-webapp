8be043711a381e102b2485d379e53838d49774ef
vaichu <vaishnavvaichu123@gmail.com>
2021-12-27 16:50:26 +0530

    added prop and changes due to function change

--- src/components/Openable.tsx
+++ src/components/Openable.tsx
@@ -1,18 +1,20 @@
 import React from "react";
-import { useClickOutsideEvent } from '../functions/hooks/useClickOutsideEvent';
+import { clickOutsideEvent } from '../functions/domEvents';
 
 type OpenableProps = {
     children?: React.ReactNode,
     className?: string,
     style?: React.CSSProperties,
     open: [boolean, (a: boolean) => void],
+    closeOnClickOutside?: boolean,
     clickOutsideCloseException?: React.MutableRefObject<HTMLElement>[],
 
 }
 
-const Openable = function ({ children, className, style, open, clickOutsideCloseException }: OpenableProps): JSX.Element {
+const Openable = function ({ children, className, style, open, clickOutsideCloseException, closeOnClickOutside = true }: OpenableProps): JSX.Element {
     const ref = React.useRef<HTMLDivElement>(null!);
-    useClickOutsideEvent(ref, () => open[1](false), clickOutsideCloseException);
+    if (closeOnClickOutside)
+        clickOutsideEvent(ref, () => open[1](false), clickOutsideCloseException);
     return (< div ref={ref} className={className} style={{ ...style, display: !open[0] ? "none" : "revert" }
     } >
         {
