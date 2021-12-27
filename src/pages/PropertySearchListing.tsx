54e337702a1d59eab84b63f38d1e11d658b8f01b
vaichu <vaishnavvaichu123@gmail.com>
2021-12-27 11:29:33 +0530

    changes in component

--- src/pages/PropertySearchListing.tsx
+++ src/pages/PropertySearchListing.tsx
@@ -4,17 +4,12 @@ import FilterModel from '../components/FilterModal';
 import style from './PropertySearchListing.module.scss';
 import Layout from './Layout';
 import RadioButton from '../components/Form/components/Radiobutton';
-import { useClickOutsideEvent } from '../functions/hooks/useClickOutsideEvent';
-import { useOpenable, Openable } from '../functions/hooks/useOpenable';
+import { Openable } from '../components/Openable';
 
 export default function (): JSX.Element {
     const [filterOpen, setFilterOpen] = React.useState(false);
     const sortButtonRef = React.useRef<HTMLButtonElement>(null!);
-    const sortMenu = useOpenable({ clickOutsideCloseException: [sortButtonRef] })
-
-    React.useEffect(() => {
-        //sortMenu.setClickOutsideExceptions([sortButtonRef.current])
-    }, []);
+    const [openSort, setOpenSort] = React.useState(false);
     return (
         <Layout>
             {filterOpen &&
@@ -27,12 +22,12 @@ export default function (): JSX.Element {
                 </div>
                 <div className={style["btn-container"]}>
                     <button onClick={() => { setFilterOpen(true); }}><i className="fas fa-sliders-h"></i></button>
-                    <button ref={sortButtonRef} onClick={() => { sortMenu.setOpen(!sortMenu.isOpen) }}>
+                    <button ref={sortButtonRef} onClick={() => { setOpenSort(!openSort) }}>
                         <i className="fas fa-sort"></i>
 
                     </button>
                 </div>
-                <Openable className={style["sort-popup"]} {...sortMenu.props}>
+                <Openable className={style["sort-popup"]} open={[openSort, setOpenSort]} clickOutsideCloseException={[sortButtonRef]}>
                     <RadioButton name="sort" values={["Default", "Recent"]} />
                 </Openable>
                 <div className={style["filter-wrapper"]} style={{ display: !filterOpen ? "none" : "revert" }}>
