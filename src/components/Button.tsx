82ecca0f67b5bf977eff18a9b53fbeb79fd18618
vaichu <vaishnavvaichu123@gmail.com>
2021-12-29 18:31:45 +0530

    avoid undefined

--- src/components/Button.tsx
+++ src/components/Button.tsx
@@ -5,11 +5,11 @@ interface props extends React.HTMLAttributes<HTMLButtonElement> {
     onClick?: () => void;
     className?: string
 }
-export function ButtonHollow({ label, onClick = () => { }, className }: props): JSX.Element {
+export function ButtonHollow({ label, onClick = () => { }, className = "" }: props): JSX.Element {
     return <button onClick={onClick} className={"btn-smoothround-hollow " + className}>{label}</button>
 
 }
-export function ButtonSolid({ label, onClick = () => { }, className }: props): JSX.Element {
+export function ButtonSolid({ label, onClick = () => { }, className="" }: props): JSX.Element {
     return <button onClick={onClick} className={"btn-smoothround-solid " + className}>{label}</button>
 
 }
