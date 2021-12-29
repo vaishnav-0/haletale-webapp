55ea3dbf5f561a6505d5ce55af8ec6bd9e50d1c2
vaichu <vaishnavvaichu123@gmail.com>
2021-12-29 16:12:01 +0530

    generalized components

--- src/components/Button.tsx
+++ src/components/Button.tsx
@@ -1,14 +1,15 @@
 import React from 'react';
 import "./Button.scss";
-type props = {
+interface props extends React.HTMLAttributes<HTMLButtonElement> {
     label: string;
     onClick?: () => void;
+    className?: string
 }
-export function ButtonHollow({ label, onClick = () => { } }: props): JSX.Element {
-    return <button onClick={onClick} className="btn-smoothround-hollow">{label}</button>
+export function ButtonHollow({ label, onClick = () => { }, className }: props): JSX.Element {
+    return <button onClick={onClick} className={"btn-smoothround-hollow " + className}>{label}</button>
 
 }
-export function ButtonSolid({ label , onClick = () => { } }: props): JSX.Element {
-    return <button onClick={onClick} className="btn-smoothround-solid">{label}</button>
+export function ButtonSolid({ label, onClick = () => { }, className }: props): JSX.Element {
+    return <button onClick={onClick} className={"btn-smoothround-solid " + className}>{label}</button>
 
 }
