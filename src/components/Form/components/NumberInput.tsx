a8bced0942196cd22bc323ed609baed4489c6996
vaichu <vaishnavvaichu123@gmail.com>
2021-12-29 19:04:40 +0530

    button inactive style

--- src/components/Form/components/NumberInput.tsx
+++ src/components/Form/components/NumberInput.tsx
@@ -9,15 +9,21 @@ export function NumberInput({ init = 0, min = 0, max }: props): JSX.Element {
     const [count, setCount] = React.useState(0)
     return (
         <div className={style["container"]}>
-            <button type="button" onClick={() => {
-                count > min && setCount(count - 1);
-            }}><i className="fas fa-plus"></i></button>
+            <button type="button"
+                className={count === min ? style["inactive"] : ""}
+                onClick={() => {
+                    count > min && setCount(count - 1);
+                }}><i className="fas fa-plus"></i>
+            </button>
             <div className={style["count"]}>
                 {count}
             </div>
-            <button type="button" onClick={() => {
-                (max != null && count == max) || setCount(count + 1)
-            }}><i className="fas fa-minus"></i></button>
+            <button type="button"
+                className={count === max ? style["inactive"] : ""}
+                onClick={() => {
+                    (max != null && count == max) || setCount(count + 1)
+                }}><i className="fas fa-minus"></i>
+            </button>
         </div>
     );
 }
