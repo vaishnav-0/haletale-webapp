d2d3a61675146a669bd65f8cf10feb408deffbb1
vaichu <vaishnavvaichu123@gmail.com>
2021-12-29 18:53:23 +0530

    prevent default action(submit)

--- src/components/Form/components/NumberInput.tsx
+++ src/components/Form/components/NumberInput.tsx
@@ -9,13 +9,13 @@ export function NumberInput({ init = 0, min = 0, max }: props): JSX.Element {
     const [count, setCount] = React.useState(0)
     return (
         <div className={style["container"]}>
-            <button onClick={() => {
+            <button type="button" onClick={() => {
                 count > min && setCount(count - 1);
             }}><i className="fas fa-plus"></i></button>
             <div className={style["count"]}>
                 {count}
             </div>
-            <button onClick={() => {
+            <button type="button" onClick={() => {
                 (max != null && count == max) || setCount(count + 1)
             }}><i className="fas fa-minus"></i></button>
         </div>
