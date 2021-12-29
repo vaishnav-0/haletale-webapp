3c35791ca262567f81dd033a41e310408bbcfe0a
vaichu <vaishnavvaichu123@gmail.com>
2021-12-29 19:57:15 +0530

    ability to add labels

--- src/components/Form/components/RadiobuttonGroup.tsx
+++ src/components/Form/components/RadiobuttonGroup.tsx
@@ -4,16 +4,28 @@ import RadioButton from './RadioButton';
 
 type props = {
     name: string
-    values: string[]
+    values: string[] | { [index: string]: string }
 }
 const RadioButtonGroup = function ({ name, values }: props): JSX.Element {
-    return (<>
-        {values.map(e => {
+    let elements = [];
+    if (Array.isArray(values))
+        elements = values.map(e => {
             const id = Math.random().toString(36).substr(2, 5);
             return (
                 <RadioButton name={name} value={e} />
             );
-        })}
+        });
+    else
+        elements = Object.entries(values).map(([value, label]) => {
+            const id = Math.random().toString(36).substr(2, 5);
+            return (
+                <RadioButton name={name} value={value} label={label} />
+            );
+        })
+    return (<>
+        {
+            elements
+        }
     </>
     );
 }
