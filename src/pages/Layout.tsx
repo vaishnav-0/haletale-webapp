6c402835c771bfa8a7690f5541d5d1e81515f782
vaichu <vaishnavvaichu123@gmail.com>
2021-12-29 16:09:21 +0530

    style moved to appropriate file

--- src/pages/Layout.tsx
+++ src/pages/Layout.tsx
@@ -2,13 +2,15 @@ import React from 'react';
 import Header from '../components/Header';
 import Body from '../components/Body';
 import Footer from '../components/Footer';
+import style from './Layout.module.scss';
+
 type props = {
     children: React.ReactNode,
     footer?: boolean
 }
 function Layout({ children, footer }: props): JSX.Element {
     return (
-        <div className="VP">
+        <div className={style["VP"]}>
             <Header />
             <Body>
                 {children}
