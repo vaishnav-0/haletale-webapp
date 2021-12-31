6febdb2cbc7c2cf5dc724db47deb13314a238c0c
vaichu <vaishnavvaichu123@gmail.com>
2021-12-31 20:52:57 +0530

    added animation

--- src/functions/animations.tsx
+++ src/functions/animations.tsx
@@ -25,6 +25,28 @@ export function expand(ref: React.MutableRefObject<HTMLElement>,
         [{ property: "height", duration: duration, timing: timing, delay: delay }],
         { height: auto ? sectionHeight + 'px' : "" }, parseFloat(duration), disabled);
 }
+export function zoomIn(ref: React.MutableRefObject<HTMLElement>,
+    { duration = "0.5s", timing = "ease-in", delay = "0s" }: transition = { duration: "0.5s", timing: "ease-in", delay: "0s" },
+    disabled = false
+) {
+    applyValues(ref,
+        [
+            { property: "transform", duration: duration, timing: timing, delay: delay },
+            { property: "opacity", duration: duration, timing: timing, delay: delay }
+        ],
+        { transform: "scale(1)", opacity: 1 }, parseFloat(duration), disabled);
+}
+export function zoomOut(ref: React.MutableRefObject<HTMLElement>,
+    { duration = "0.5s", timing = "ease-in", delay = "0s" }: transition = { duration: "0.5s", timing: "ease-in", delay: "0s" },
+    disabled = false
+) {
+    applyValues(ref,
+        [
+            { property: "transform", duration: duration, timing: timing, delay: delay },
+            { property: "opacity", duration: duration, timing: timing, delay: delay }
+        ],
+        { transform: "scale(0)", opacity: 0 }, parseFloat(duration), disabled);
+}
 
 interface transition {
     property?: string,
@@ -36,7 +58,7 @@ function applyTransition(ref: React.MutableRefObject<HTMLElement>,
     { property, duration = "0.5s", timing = "ease-in", delay = "0s" }: transition
 ) {
     let transition = `${property} ${duration} ${timing} ${delay}`;
-    ref.current.style.transition = (ref.current.style.transition === "" ? "" : ",") + transition;
+    ref.current.style.transition += (ref.current.style.transition === "" ? "" : ",") + transition;
 }
 function applyTransitions(ref: React.MutableRefObject<HTMLElement>,
     transitions: transition[]
