84384094d1a5cfe450d4e523e208437447e1ae08
vaichu <vaishnavvaichu123@gmail.com>
2022-01-01 19:14:52 +0530

    type definition

--- src/components/Button.tsx
+++ src/components/Button.tsx
@@ -1,15 +1,16 @@
 import React from 'react';
 import "./Button.scss";
-interface props extends React.HTMLAttributes<HTMLButtonElement> {
+interface propsType extends React.HTMLAttributes<HTMLButtonElement> {
     label: string;
     onClick?: () => void;
     className?: string
 }
-export function ButtonHollow({ label, onClick = () => { }, className = "" }: props): JSX.Element {
-    return <button onClick={onClick} className={"btn-smoothround-hollow " + className}>{label}</button>
+export type ButtonComponentType = (props: propsType) => JSX.Element
+export const ButtonHollow: ButtonComponentType = function ({ label, onClick = () => { }, className = "" }) {
+    return <button onClick={onClick} className={"btn-smoothround-hollow " + className}> {label}</button >
 
 }
-export function ButtonSolid({ label, onClick = () => { }, className="" }: props): JSX.Element {
+export const ButtonSolid: ButtonComponentType = function ({ label, onClick = () => { }, className = "" }) {
     return <button onClick={onClick} className={"btn-smoothround-solid " + className}>{label}</button>
 
 }
