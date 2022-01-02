775962c0ca400fb340bb75d6084940a59b0638cf
vaichu <vaishnavvaichu123@gmail.com>
2022-01-02 09:40:09 +0530

    better structure and layout

--- src/components/Form/components/ToggleButtons.tsx
+++ src/components/Form/components/ToggleButtons.tsx
@@ -6,24 +6,28 @@ interface props extends React.DetailedHTMLProps<React.HTMLAttributes<HTMLDivElem
     label?: string
     type?: "radio" | "checkbox"
 }
-function ToggleButton({ name, value, label, type, key }: props): JSX.Element {
+function ToggleButton({ name, value, label, type, key, children }: props): JSX.Element {
     const id = Math.random().toString(36).substr(2, 5);
     return (
-        <div key={key} className={style["radio-item"]}>
-            {
-                type === "radio" ?
-                    <input type="radio" id={id} name={name} value={value} />
-                    :
-                    <input type="checkbox" id={id} name={name} value={value} />
-            }
-            <div className={style["checked"]}>
-                <i className={`far ${type === "radio" ? "fa-dot-circle" : "fa-check-square"}`} />
+        <label htmlFor={id}>
+            <div key={key} className={style["radio-item"]}>
+                {
+                    type === "radio" ?
+                        <input type="radio" id={id} name={name} value={value} />
+                        :
+                        <input type="checkbox" id={id} name={name} value={value} />
+                }
+                <div className={style["checked"]}>
+                    <i className={`far ${type === "radio" ? "fa-dot-circle" : "fa-check-square"}`} />
+                </div>
+                <div className={style["unchecked"]}>
+                    <i className={`far ${type === "radio" ? "fa-circle" : "fa-square"}`} />
+                </div>
+                <div className={style["label"]}>
+                    {children ?? label ?? value}
+                </div>
             </div>
-            <div className={style["unchecked"]}>
-                <i className={`far ${type === "radio" ? "fa-circle" : "fa-square"}`} />
-            </div>
-            <label htmlFor={id}>{label ? label : value}</label>
-        </div>
+        </label>
     );
 }
 export function RadioButton(props: props): JSX.Element {
