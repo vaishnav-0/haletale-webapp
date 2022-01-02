ed2ef40d9ea66e37391fc0d0e153053aa14317cd
vaichu <vaishnavvaichu123@gmail.com>
2022-01-02 09:36:13 +0530

    added ImageUpload component

---
+++ src/components/Form/components/Images.tsx
@@ -0,0 +1,211 @@
+import React from 'react';
+import style from './Images.module.scss';
+import ImageUploading, { ImageListType, ResolutionType } from "../../../libs/react-images-uploading/src";
+import { isResolutionValid } from '../../../libs/react-images-uploading/src/validation';
+import { getImage } from '../../../libs/react-images-uploading/src/utils';
+import { Drag } from '../../../functions/DragEvent';
+import Cropper from "react-cropper";
+import "cropperjs/dist/cropper.css";
+
+interface props {
+    max?: number,
+    multiple?: boolean,
+    acceptType?: string[],
+    maxFileSize?: number,
+    resolutionType?: ResolutionType,
+    resolutionWidth?: number
+    resolutionHeight?: number
+    rejectOnResolutionError?: boolean
+}
+
+export function ImageUpload({ max = 1000, multiple = true, acceptType = ['jpg', 'gif', 'png', 'jpeg'],
+    maxFileSize, resolutionType,
+    resolutionHeight, resolutionWidth, rejectOnResolutionError }: props): JSX.Element {
+    const CUSTOM_DATA_URL = "dataUrl";
+    const [images, setImages] = React.useState<ImageListType>([]);
+    const [dragging, setDragging] = React.useState<boolean>(false);
+    const [resErrors, setResErrors] = React.useState<Set<number>>(new Set());
+    const [editor, setEditor] = React.useState<number | false>(false);
+    const imgRefs = React.useRef<HTMLImageElement[]>([]);//why?
+    const divRef = React.useRef<HTMLDivElement>(null!);
+    const cropperRef = React.useRef<HTMLImageElement>(null);
+    React.useEffect(() => {
+        const drag = new Drag(divRef.current);
+        const enterHandler = (e: Event) => {
+            setDragging(true);
+        }
+        const leaveHandler = (e: Event) => {
+            setDragging(false);
+        }
+        divRef.current.addEventListener("drag:enter", enterHandler)
+        divRef.current.addEventListener("drag:leave", leaveHandler)
+        return () => {
+            drag.unsuscribe();
+            divRef.current.removeEventListener("drag:enter", enterHandler)
+            divRef.current.removeEventListener("drag:leave", leaveHandler)
+        }
+    }, []);
+    const onChange = (imageList: ImageListType, addUpdateIndex: number[] | undefined) => {
+        if (!rejectOnResolutionError) {
+            addUpdateIndex?.forEach(async (i) => {
+                if (resolutionType) {
+                    const image = await getImage(imageList[i].file as File);
+                    const checkRes = isResolutionValid(
+                        image,
+                        resolutionType,
+                        resolutionWidth,
+                        resolutionHeight
+                    );
+                    if (!checkRes) {
+                        setResErrors(prev => (new Set(prev)).add(i));
+                    }
+                    else {
+                        if (resErrors.has(i))
+                            setResErrors(prev => {
+                                const s = new Set(prev);
+                                s.delete(i);
+                                return s;
+                            });
+                    }
+                }
+            });
+        }
+        setImages(imageList);
+    };
+    const setEditedImage = (i: number) => {
+        const cropper: any = (cropperRef?.current as any)?.cropper;
+        if (cropper) {
+            const img: { [CUSTOM_DATA_URL]?: string, file?: File } = {};
+            cropper.getCroppedCanvas().toBlob((blob: Blob) => {
+                const reader = new FileReader();
+                reader.readAsDataURL(blob);
+                reader.onerror = () => {
+                    setEditor(false);
+                }
+                reader.onload = () => {
+                    img[CUSTOM_DATA_URL] = reader.result as string;
+                    img.file = new File([blob], (images[i].file as File).name);
+                    images[i] = img;
+                    onChange(images, [i]);
+                    setEditor(false);
+                }
+            });
+        }
+    }
+    console.log(resErrors);
+    return (
+        <ImageUploading
+            multiple={multiple}
+            value={images}
+            onChange={onChange}
+            maxNumber={max}
+            dataURLKey={CUSTOM_DATA_URL}
+            acceptType={acceptType}
+            maxFileSize={maxFileSize}
+            {...rejectOnResolutionError ?
+                {
+                    resolutionType,
+                    resolutionHeight,
+                    resolutionWidth,
+                } : {}
+            }
+        >
+            {({
+                imageList,
+                onImageUpload,
+                onImageRemoveAll,
+                onImageUpdate,
+                onImageRemove,
+                isDragging,
+                dragProps,
+                errors
+            }) => {
+                return (
+                    <div ref={divRef} className={style["imageupload-wrapper"]}>
+                        <div className={style["imageupload-container-wrapper"]}>
+                            <div onDrop={dragProps.onDrop}
+                                onDragOver={dragProps.onDragOver} className={style["drag-overlay"]} style={{ display: dragging ? "" : "none" }} />
+                            <div onDrop={dragProps.onDrop}
+                                onDragOver={dragProps.onDragOver}
+                                className={`${style["imageupload-container"]} ${dragging ? style["dragging"] : ""} ${imageList.length === 0 ? style["empty"] : ""}`}
+                            >
+                                <div className={style["imagelist-container"]}>
+                                    {imageList.map((image, index) => (
+                                        <div key={index}
+                                            className={`${style["image-item"]} ${(!rejectOnResolutionError && resErrors.has(index)) ? style["image-item-error"] : ""}`}>
+                                            <div key={index} className={style["image-element"]}>
+                                                <img ref={el => imgRefs.current[index] = el as HTMLImageElement}
+                                                    src={image[CUSTOM_DATA_URL]} alt="" />
+                                                <button type="button" onClick={() => onImageRemove(index)} className={style["image-close-btn"]}>
+                                                    <i className="fas fa-times" />
+                                                </button>
+                                                <button type="button" onClick={() => onImageUpdate(index)} className={style["image-update-btn"]}>
+                                                    <i className="fas fa-redo" />
+                                                </button>
+                                                <button type="button" onClick={() => { setEditor(index) }} className={style["image-edit-btn"]}>
+                                                    <i className="far fa-edit" />
+                                                </button>
+                                            </div>
+                                        </div>
+                                    ))}
+                                    {(editor !== false) &&
+                                        <div className={style["image-editor-container"]}>
+                                            <Cropper
+                                                className={style["image-editor"]}
+                                                aspectRatio={16 / 9}
+                                                guides={false}
+                                                src={images[editor][CUSTOM_DATA_URL]}
+                                                ref={cropperRef}
+                                            >
+
+                                            </Cropper>
+                                            <div className={style["edit-tool-container"]}>
+                                                <button
+                                                    type="button"
+                                                    onClick={() => (cropperRef.current as any)?.cropper.rotate(-90)}
+                                                    className={style["rotate-left"]}
+                                                >
+                                                    <i className="fas fa-undo" />
+
+                                                </button>
+                                                <button
+                                                    type="button"
+                                                    onClick={() => (cropperRef.current as any)?.cropper.rotate(90)}
+                                                    className={style["rotate-right"]}
+                                                >
+                                                    <i className="fas fa-redo" />
+                                                </button>
+                                            </div>
+                                            <button type="button" onClick={() => setEditor(false)} className={style["image-editor-close-btn"]}>
+                                                <i className="fas fa-times" />
+                                            </button>
+                                            <button type="button" onClick={() => setEditedImage(editor)} className={style["image-editor-confirm-btn"]}>
+                                                <i className="fas fa-check" />
+                                            </button>
+                                        </div>
+                                    }
+                                </div>
+                            </div>
+                        </div>
+
+                        <div className={style["imageupload-btn-container"]}>
+                            <button
+                                type="button"
+                                onClick={onImageUpload}
+                                className={style["add-btn"]}
+                            >
+                                Add image
+                            </button>
+                            <button type="button"
+                                className={style["remove-btn"]}
+                                onClick={onImageRemoveAll}>Remove all
+                            </button>
+                        </div>
+
+                    </div>
+                )
+            }
+            }
+        </ImageUploading >
+    );
+}
