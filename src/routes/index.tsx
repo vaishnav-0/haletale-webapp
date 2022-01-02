cff801d308fd044ab1c05eb08a890cdb07ca5370
vaichu <vaishnavvaichu123@gmail.com>
2022-01-02 09:41:20 +0530

    added new routes

--- src/routes/index.tsx
+++ src/routes/index.tsx
@@ -6,12 +6,17 @@ import {
 } from "react-router-dom";
 import HomePage from "../pages/Home";
 import PropertyListing from "../pages/PropertySearchListing";
+import AddProperty from '../pages/AddProperty';
+import Signup from "../pages/SignUp";
+
 export default function () {
     return (
         <BrowserRouter>
             <Routes>
                 <Route path="/" element={< HomePage />} />
                 <Route path="/properties" element={< PropertyListing />} />
+                <Route path="/addProperty" element={< AddProperty />} />
+                <Route path="/signup" element={< Signup />} />
 
                 <Route
                     path="*"
