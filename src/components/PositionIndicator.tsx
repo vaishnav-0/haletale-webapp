dc2c0250ab5490c479ebaa8ecbbde2d8815c1fa5
vaichu <vaishnavvaichu123@gmail.com>
2022-01-04 23:19:37 +0530

    Position indicator click event handling

--- src/components/PositionIndicator.tsx
+++ src/components/PositionIndicator.tsx
@@ -1,15 +1,53 @@
 import React from 'react';
-import style from "./PositionIndicator.module.scss";
+import cssStyle from "./PositionIndicator.module.scss";
 type PropType = {
     count: number,
-    position: number
+    position?: number
+    onChange?: (pos: number) => void
+    className?: string
+    style?: React.CSSProperties;
 }
-export default function ({ count, position }: PropType): JSX.Element {
-    console.log(Array(count));
+export default function ({ count, position = 0, onChange = () => { }, className, style }: PropType): JSX.Element | null {
+    if (count < 1) {
+        return null;
+    }
+    const [pos, setPos] = React.useState(0);
+    const posRef = React.useRef(pos);
+    const dotRefs = React.useRef<(HTMLDivElement | null)[]>([])
+    React.useEffect(() => {
+        setPos(position);
+    }, [position]);
+    React.useEffect(() => {
+        posRef.current = pos;
+    }, [pos]);
+    console.log(pos);
+    React.useEffect(() => {
+        const refs = dotRefs.current;
+        const eventHandlers: (() => void)[] = []
+        refs.forEach((e, i) => {
+            console.log(e)
+            const clickHandler = function () {
+                console.log(i, posRef.current)
+                console.log("c");
+                if (posRef.current !== i) {
+                    setPos(i);
+                    onChange(i);
+                }
+            }
+            eventHandlers.push(clickHandler)
+            e?.addEventListener('click', clickHandler);
+        });
+        return () => {
+            refs.forEach((e, i) => {
+                e?.removeEventListener('click', eventHandlers[i]);
+            });
+        }
+
+    }, [count]);
     return (
-        <div className={style["indicator-container"]}>
+        <div style={style} className={cssStyle["indicator-container"] + " " + className ?? ""}>
             {
-                [...Array(count)].map((e, i) => <div className={position === i + 1 ? style["active"] : ""}></div>)
+                [...Array(count)].map((e, i) => <div key={i} ref={el => dotRefs.current[i] = el} className={pos === i ? cssStyle["active"] : ""}></div>)
             }
         </div>
     );
