0f99130476eac7de87c03bfffb4c57c96d322a53
vaichu <vaishnavvaichu123@gmail.com>
2022-01-04 23:16:39 +0530

    children support for component

--- src/components/Button.tsx
+++ src/components/Button.tsx
@@ -1,16 +1,20 @@
 import React from 'react';
 import "./Button.scss";
 interface propsType extends React.DetailedHTMLProps<React.ButtonHTMLAttributes<HTMLButtonElement>, HTMLButtonElement> {
-    label: string;
+    label?: string;
     onClick?: () => void;
     className?: string
 }
 export type ButtonComponentType = React.FC<propsType>
-export const ButtonHollow: ButtonComponentType = function ({ label, onClick = () => { }, className = "", ...rest }) {
-    return <button onClick={onClick} className={"btn-smoothround-hollow " + className} {...rest}> {label}</button >
+export const ButtonHollow: ButtonComponentType = function ({ children, onClick = () => { }, className = "", ...rest }) {
+    return <button onClick={onClick} className={"btn-smoothround-hollow " + className} {...rest}>
+        {children}
+    </button >
 
 }
-export const ButtonSolid: ButtonComponentType = function ({ label, onClick = () => { }, className = "", ...rest }) {
-    return <button onClick={onClick} className={"btn-smoothround-solid " + className} {...rest}>{label}</button>
+export const ButtonSolid: ButtonComponentType = function ({ children, onClick = () => { }, className = "", ...rest }) {
+    return <button onClick={onClick} className={"btn-smoothround-solid " + className} {...rest}>
+        {children}
+    </button>
 
 }
