fea93bd22a94abd1de91e083a97aed81eef19aeb
vaichu <vaishnavvaichu123@gmail.com>
2022-01-07 22:47:13 +0530

    new props

--- src/components/ImageSlider.tsx
+++ src/components/ImageSlider.tsx
@@ -1,44 +1,51 @@
 import React from 'react';
 import style from './ImageSlider.module.scss';
-import propertyImg1 from "../assets/images/property.jpeg";
-import propertyImg2 from "../assets/images/housewide.jpg";
 import { Carousel } from 'react-responsive-carousel';
 import "react-responsive-carousel/lib/styles/carousel.min.css";
 import PositionIndicator from '../components/PositionIndicator';
-type propType = {
+interface propType {
     aspectRatio: number;
     indicatorClassName?: string;
-    className?: string
+    className?: string;
+    onClick?: () => void;
+    imgSrc: string[];
+    showArrows?: boolean;
+    showIndicators?: boolean
+    showStatus?: boolean;
+    infiniteLoop?: boolean;
+    onChange?: () => void;
+    selectedItem?: number | undefined
 }
-export default function ({ aspectRatio = 16 / 9, indicatorClassName = "", className = "" }: propType) {
+export default function ({ aspectRatio = 16 / 9, indicatorClassName = "",
+    showArrows = false, showIndicators = false, showStatus = false, infiniteLoop = true,
+    className = "", onChange = () => { }, onClick = () => { }, selectedItem = 0, imgSrc = [] }: propType) {
     const [currentImage, setCurrentImage] = React.useState(0);
     return (
-        <div className={style["slider-container"]}>
+        <div onClick={onClick} className={style["slider-container"]}>
             <PositionIndicator style={{ position: "absolute", zIndex: "10" }} className={indicatorClassName} onChange={(i) => setCurrentImage(i)}
                 position={currentImage} count={2} />
             <Carousel
                 className={className}
+                showArrows={showArrows}
                 showThumbs={false}
-                showArrows={false}
-                showIndicators={false}
-                showStatus={false}
+                showIndicators={showIndicators}
+                showStatus={showStatus}
                 emulateTouch={true}
-                infiniteLoop={true}
+                infiniteLoop={infiniteLoop}
                 onChange={(i) => setCurrentImage(i)}
                 selectedItem={currentImage}
             >
-                <div className={style["image-slider"]}>
-                    <div style={{ paddingBottom: 100 / aspectRatio + "%" }} className={style["aspect-ratio-container"]} />
-                    <div className={style["image-slider-container"]}>
-                        <img src={propertyImg1} />
-                    </div>
-                </div>
-                <div className={style["image-slider"]}>
-                    <div className={style["aspect-ratio-container"]} />
-                    <div className={style["image-slider-container"]}>
-                        <img src={propertyImg2} />
-                    </div>
-                </div>
+                {
+                    imgSrc.map(i => {
+                        return <div className={style["image-slider"]}>
+
+                            <div style={{ paddingBottom: 100 / aspectRatio + "%" }} className={style["aspect-ratio-container"]} />
+                            <div className={style["image-slider-container"]}>
+                                <img src={i} />
+                            </div>
+                        </div>
+                    })
+                }
             </Carousel>
         </div>
     );
