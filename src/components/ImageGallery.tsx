7380d0f8cca8abc18048c9eef00e59465adcf478
vaichu <vaishnavvaichu123@gmail.com>
2022-01-07 22:55:16 +0530

    ImageGallary

---
+++ src/components/ImageGallery.tsx
@@ -0,0 +1,49 @@
+import React from 'react';
+import style from './ImageGallery.module.scss';
+import { Carousel } from 'react-responsive-carousel';
+import "react-responsive-carousel/lib/styles/carousel.min.css";
+import PositionIndicator from '../components/PositionIndicator';
+interface propType {
+    indicatorClassName?: string;
+    className?: string;
+    onClick?: () => void;
+    imgSrc: string[];
+    showThumbs?: boolean;
+    showArrows?: boolean;
+    showIndicators?: boolean
+    showStatus?: boolean;
+    infiniteLoop?: boolean;
+    onChange?: () => void;
+    selectedItem?: number | undefined
+}
+export default function ({ indicatorClassName = "",
+    showThumbs = false, showArrows = false, showIndicators = false, showStatus = false, infiniteLoop = true,
+    className = "", onChange = () => { }, onClick = () => { }, selectedItem = 0, imgSrc = [] }: propType) {
+    const [currentImage, setCurrentImage] = React.useState(0);
+    return (
+        <div onClick={onClick} className={style["slider-container"]}>
+            <PositionIndicator style={{ position: "absolute", zIndex: "10" }} className={indicatorClassName + " " + style["position-indicator"]} onChange={(i) => setCurrentImage(i)}
+                position={currentImage} count={2} />
+            <Carousel
+                className={className}
+                showThumbs={showThumbs}
+                showArrows={showArrows}
+                showIndicators={showIndicators}
+                showStatus={showStatus}
+                emulateTouch={true}
+                infiniteLoop={infiniteLoop}
+                onChange={(i) => setCurrentImage(i)}
+                selectedItem={currentImage}
+                dynamicHeight={true}
+            >
+                {
+                    imgSrc.map(i => {
+                        return <div className={style["image-container"]}>
+                            <img src={i} />
+                        </div>
+                    })
+                }
+            </Carousel>
+        </div>
+    );
+}
