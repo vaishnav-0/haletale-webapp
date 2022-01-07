5c522e3149c2fe9ba2089c8df652b03d9c555479
vaichu <vaishnavvaichu123@gmail.com>
2022-01-07 22:56:03 +0530

    Temp links

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -1,20 +1,27 @@
 import React from 'react';
 import "./Footer.scss";
-export default function (): JSX.Element {
+import { useNavigate } from "react-router-dom";
+
+export default function Footer(): JSX.Element {
+    let navigate = useNavigate();
     return (
         <div className="footer">
             <div className="footer-top">
                 <li>
                     <ul>Company</ul>
                     <ul><a href="#">Home</a></ul>
-                    <ul><a href="#">Sign In / Sign Up</a></ul>
+                    <ul><a onClick={() => {
+                        navigate("/signup");
+                    }}>Sign In / Sign Up</a></ul>
                     <ul><a href="#">About Us</a></ul>
                     <ul><a href="#">Contact Us</a></ul>
                 </li>
                 <li>
                     <ul>Quick Links</ul>
 
-                    <ul><a href="#">propertyfile</a></ul>
+                    <ul><a onClick={() => {
+                        navigate("/addProperty");
+                    }} >propertyfile</a></ul>
                     <ul><a href="#">Notifications</a></ul>
                     <ul><a href="#">Your Bookings</a></ul>
                     <ul><a href="#">Wishlist</a></ul>
