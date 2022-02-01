27728e008f3a16680202c6eb1ec17bdb5a401864
vaichu <vaishnavvaichu123@gmail.com>
2022-02-01 20:12:01 +0530

    scss module port

--- src/components/Button.tsx
+++ src/components/Button.tsx
@@ -1,18 +1,18 @@
 import React from 'react';
-import "./Button.scss";
+import style from "./Button.module.scss";
 interface PropsType extends React.DetailedHTMLProps<React.ButtonHTMLAttributes<HTMLButtonElement>, HTMLButtonElement> {
     label?: string;
     className?: string
 }
 export type ButtonComponentType = React.FC<PropsType>
 export const ButtonHollow: ButtonComponentType = function ({ children, onClick = () => { }, className = "", ...rest }) {
-    return <button onClick={onClick} className={"btn-smoothround-hollow " + className} {...rest}>
+    return <button onClick={onClick} className={style["btn-smoothround-hollow"] + " " + className} {...rest}>
         {children}
     </button >
 
 }
 export const ButtonSolid: ButtonComponentType = function ({ children, onClick = () => { }, className = "", ...rest }) {
-    return <button onClick={onClick} className={"btn-smoothround-solid " + className} {...rest}>
+    return <button onClick={onClick} className={style["btn-smoothround-solid"] + " " + className} {...rest}>
         {children}
     </button>
 
