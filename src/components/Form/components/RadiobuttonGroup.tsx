25116a9a7a1588294955e6c9b360420d1d325f90
vaichu <vaishnavvaichu123@gmail.com>
2022-02-01 21:45:11 +0530

    simplified

--- src/components/Form/components/RadiobuttonGroup.tsx
+++ src/components/Form/components/RadiobuttonGroup.tsx
@@ -10,29 +10,17 @@ export interface PropsType extends InputPropsType {
 }
 
 const RadioButtonGroup = React.forwardRef<HTMLInputElement, PropsType>(({ name, values, type, defaultValue, ...rest }: PropsType, ref) => {
-    let elements = [];
-    if (Array.isArray(values))
-        elements = values.map((e, i) => {
-            const id = Math.random().toString(36).substr(2, 5);
-            return (
-                <RadioButton key={i} name={name} {...rest} value={e} ref={ref} />
-            );
-        });
-    else
-        elements = Object.entries(values).map(([value, label], i) => {
-            const id = Math.random().toString(36).substr(2, 5);
+    const values_ = Array.isArray(values) ? Object.fromEntries(values.map(e => [e, e])) : values;
+    return <>
+        {Object.entries(values_).map(([value, label], i) => {
             return (
                 <RadioButton key={i} name={name} {...rest} value={value} label={label} ref={ref}
                     {...defaultValue === value ? { defaultChecked: true } : {}}
                 />
             );
         })
-    return (<>
-        {
-            elements
         }
     </>
-    );
 });
 
 export { RadioButtonGroup };
