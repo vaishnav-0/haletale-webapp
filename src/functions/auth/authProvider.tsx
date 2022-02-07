20a3524fcab4b7bd115f28fba7b2efc79879f56d
vaichu <vaishnavvaichu123@gmail.com>
2022-02-07 12:25:46 +0530

    used new auth

--- src/functions/auth/authProvider.tsx
+++ src/functions/auth/authProvider.tsx
@@ -1,61 +1,15 @@
-// const AuthProvider = {
-// //     isAuthenticated: false,
-// //     signin(callback: VoidFunction) {
-// //       AuthProvider.isAuthenticated = true;
-// //       setTimeout(callback, 100);
-// //     },
-// //     signout(callback: VoidFunction) {
-// //       AuthProvider.isAuthenticated = false;
-// //       setTimeout(callback, 100);
-// //     }
-// //   };
-
-// //   export { AuthProvider };
-
-
-
 import React from 'react';
-import { useAuth } from './useAuth';
 import authContext from './authContext';
-
-
-
-// const fakeAuthProvider = {
-//     isAuthenticated: false,
-//     signin(callback: VoidFunction) {
-//       fakeAuthProvider.isAuthenticated = true;
-//       setTimeout(callback, 100); // fake async
-//     },
-//     signout(callback: VoidFunction) {
-//       fakeAuthProvider.isAuthenticated = false;
-//       setTimeout(callback, 100);
-//     }
-//   };
+import auth from './index';
 
 export default function AuthProvider({ children }: { children: React.ReactNode }) {
-    let [user, setUser] = React.useState<any>(null);
-
-    const AuthContext = useAuth();
-
-        let signin = (newUser: string, callback: VoidFunction) => {
-          return fakeAuthProvider.signin(() => {
-            setUser(newUser);
-            callback();
-          });
-        };
-
-        let signout = (callback: VoidFunction) => {
-          return fakeAuthProvider.signout(() => {
-            setUser(null);
-            callback();
-          });
-        };
-
-    let value = {
-        user,
-        signin, signout 
-    };
-
-    return <authContext.Provider value={value}>{children}</authContext.Provider>;
+  let [user, setUser] = React.useState<any>(null);
+  React.useEffect(() => {
+    auth.onAuthStateChange(
+      (user) => {
+        setUser(user);
+      })
+  }, []);
+  return <authContext.Provider value={{ user }}>{children}</authContext.Provider>;
 
 }
