6ea02ffa0ce803ad6f56d7e81da3540ddde08d7e
vaichu <vaishnavvaichu123@gmail.com>
2022-02-09 16:20:52 +0530

    support for string[] values

--- src/components/Form/components/Select.tsx
+++ src/components/Form/components/Select.tsx
@@ -4,16 +4,17 @@ import { SelectPropsType } from './types';
 export interface PropsType extends SelectPropsType {
     values: {
         [index: string]: string;
-    }
+    } | string[]
 }
 export type ElementType = typeof Select;
 
 export const Select = React.forwardRef<HTMLSelectElement, PropsType>(({ values, ...inputProps }: PropsType, ref) => {
+    const values_ = Array.isArray(values) ? Object.fromEntries(values.map(e => [e, e])) : values;
     return (
         <div className={style["box"]}>
             <select {...inputProps} ref={ref}>
                 {
-                    Object.entries(values).map(([value, label]) => {
+                    Object.entries(values_).map(([value, label]) => {
                         return < option value={value} > {label}</option>
                     })
                 }
