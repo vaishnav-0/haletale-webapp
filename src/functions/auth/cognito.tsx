abd66e4dacab520da6b356e09cca82caf2bd91df
vaichu <vaishnavvaichu123@gmail.com>
2022-02-10 19:25:30 +0530

    ported to  cognito-identity-js

--- src/functions/auth/cognito.tsx
+++ src/functions/auth/cognito.tsx
@@ -5,7 +5,6 @@ import {
     AuthenticationDetails,
     ICognitoUserPoolData
 } from 'amazon-cognito-identity-js';
-import { CognitoIdentityProvider } from '@aws-sdk/client-cognito-identity-provider';
 import { TSignUpObject, TSignInObject } from './types';
 
 const poolData: ICognitoUserPoolData = {
@@ -15,82 +14,75 @@ const poolData: ICognitoUserPoolData = {
 
 const userPool = new CognitoUserPool(poolData);
 
-const provider = new CognitoIdentityProvider({ region: 'ca-central-1' });
 
 function cognitoSignUp(params: TSignUpObject, callback: (err: Error | null, data?: any) => void): void {
 
-    const { password, ...attributesObj } = params;
-    const attributes = Object.entries(attributesObj).map(([k, v]) => Object.assign({}, { Name: k, Value: v }))
-    console.log(attributes);
-    provider.signUp({ ClientId: poolData.ClientId, Username: params.email, Password: params.password, UserAttributes: attributes },
-        (err: any, data: any) => {
-            if (err) {
-                console.log(err);
-                callback(err);
-            }
-            else {
-                callback(null, data)
-            }
+    let attributes: Array<CognitoUserAttribute> = [];
+    let attributeEmail = new CognitoUserAttribute({
+        Name: 'email',
+        Value: params.email,
+    });
+    attributes.push(attributeEmail);
+
+    if (params.name) {
+        let attributePhoneNumber = new CognitoUserAttribute({
+            Name: 'name',
+            Value: params.name,
+        });
+        attributes.push(attributePhoneNumber);
+    }
+
+    if (params.phone) {
+        let attributePhoneNumber = new CognitoUserAttribute({
+            Name: 'phone_number',
+            Value: params.phone,
+        });
+        attributes.push(attributePhoneNumber);
+    }
+    userPool.signUp(params.email, params.password, attributes, [], (err, result) => {
+        if (err) {
+            console.log(err.message || JSON.stringify(err));
+            callback(err);
+            return;
         }
-    );
-    // let attributes: Array<CognitoUserAttribute> = [];
-    // let attributeEmail = new CognitoUserAttribute({
-    //     Name: 'email',
-    //     Value: params.email,
-    // });
-    // attributes.push(attributeEmail);
+        callback(null, result)
+        console.log(result)
+        let cognitoUser = result?.user;
+        console.log('user name is ' + cognitoUser?.getUsername() + cognitoUser);
+        //callback
 
-
-    // if (params.phone) {
-    //     let attributePhoneNumber = new CognitoUserAttribute({
-    //         Name: 'phone_number',
-    //         Value: params.phone,
-    //     });
-    //     attributes.push(attributePhoneNumber);
-    // }
-    // email is used as username too
-    // userPool.signUp(params.email, params.password, attributes, [], (err, result) => {
-    //     if (err) {
-    //         alert(err.message || JSON.stringify(err));
-
-    //         return;
-    //     }
-    //     let cognitoUser = result!.user;
-    //     console.log('user name is ' + cognitoUser.getUsername() + cognitoUser);
-    //     //callback
-
-    // });
+    });
 
     return;
 }
 
 
 function cognitoSignin(params: TSignInObject, callback: Function): void {
-
-    let authenticationDetails = {
-        USERNAME: params.email,
-        PASSWORD: params.password,
+    let authenticationData = {
+        Username: params.email,
+        Password: params.password,
     };
-
+    let authenticationDetails = new AuthenticationDetails(
+        authenticationData
+    );
+    let userPool = new CognitoUserPool(poolData);
     let userData = {
         Username: params.email,
         Pool: userPool,
     };
+    let cognitoUser = new CognitoUser(userData);
+    cognitoUser.authenticateUser(authenticationDetails, {
+        onSuccess: function (result: any) {
+            let accessToken = result.getAccessToken().getJwtToken();
+            console.log(result)
+            callback(null, result);
+        },
 
-    provider.initiateAuth({ AuthFlow: "USER_PASSWORD_AUTH", AuthParameters: authenticationDetails, ClientId: poolData.ClientId });
-    // let cognitoUser = new CognitoUser(userData);
-    // cognitoUser.authenticateUser(authenticationDetails, {
-    //     onSuccess: function (result: any) {
-    //         var accessToken = result.getAccessToken().getJwtToken();
-    //         console.log(result)
-    //         callback(null, result);
-    //     },
-
-    //     onFailure: function (err: Error) {
-    //         console.log(err)
-    //         callback(err);
-    //     },
-    // });
+        onFailure: function (err: Error) {
+            console.log(err)
+            callback(err);
+        },
+    });
 }
 
 
