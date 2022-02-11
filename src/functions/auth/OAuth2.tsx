3e202ce7a2b1f245cf3aab6a69382ae03dfa3822
vaichu <vaishnavvaichu123@gmail.com>
2022-02-12 02:06:55 +0530

    pkce

--- src/functions/auth/OAuth2.tsx
+++ src/functions/auth/OAuth2.tsx
@@ -1,16 +1,22 @@
 import OAuth2Login from '../../libs/react-simple-oauth2-login-master/src/OAuth2Login';
 
 const OAuth2 = (provider: string, callback: Function) => {
-    return new OAuth2Login({
-        authorizationUrl: "https://haletale-web1.auth.ca-central-1.amazoncognito.com/oauth2/authorize",
-        responseType: "token",
-        clientId: "7qn7vujd2vnafig3494ktacae1",
-        redirectUri: "http://localhost:3000/auth",
-        scope: "email openid phone",
-        extraParams: { identity_provider: provider },
-        onSuccess: (o: { [k: string]: any }) => callback(null,o),
-        onFailure: (o: { [k: string]: any }) => callback(o),
-    })
+    return new OAuth2Login(
+        {
+            authorizationUrl: "https://haletale-web1.auth.ca-central-1.amazoncognito.com/oauth2/authorize",
+            responseType: "code",
+            clientId: "7qn7vujd2vnafig3494ktacae1",
+            redirectUri: "http://localhost:3000/auth",
+            scope: "email openid phone",
+            extraParams: { identity_provider: provider },
+            onSuccess: (o: { [k: string]: any }) => callback(null, o),
+            onFailure: (o: { [k: string]: any }) => callback(o),
+        },
+        {
+            pkce: true,
+            tokenUrl: "https://haletale-web1.auth.ca-central-1.amazoncognito.com/oauth2/token"
+        }
+    )
 }
 
 export default OAuth2
