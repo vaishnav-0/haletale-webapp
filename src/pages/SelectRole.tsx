acd57ace6889b742d958681ac2b1622f422934c6
vaichu <vaishnavvaichu123@gmail.com>
2022-02-13 19:46:08 +0530

    added graphql call

--- src/pages/SelectRole.tsx
+++ src/pages/SelectRole.tsx
@@ -4,10 +4,18 @@ import landlordIcon from '../assets/icons/key.png';
 import TenentIcon from '../assets/icons/tenant.png';
 import auth from '../functions/auth';
 import { useNavigate } from 'react-router-dom';
+import { userMutation } from '../queries';
+import { useMutation } from '@apollo/client';
+import { useAuth } from '../functions/auth/useAuth';
 export default function SelectRole() {
+    const authData = useAuth()
+    const [setRoleMutation, { data }] = useMutation(userMutation.UPDATE_USER_ROLE)
     const navigate = useNavigate();
-    const onClick = () => {
-
+    const setRole = (role: number) => {
+        setRoleMutation({ variables: { id: authData?.user!.user_id, role_id: role } }).then(d => {
+            console.log(d,"asdasdasd");
+            auth.refreshSession();
+        }).catch(e => console.log(e))
     }
     React.useEffect(() => {
         auth.onAuthStateChange((user) => {
@@ -18,7 +26,7 @@ export default function SelectRole() {
     return <div className={style["container"]}>
         <div className={style["role-container"]}>
             <div className={style["role-item"]}>
-                <button>
+                <button onClick={() => setRole(4)}>
                     <img src={landlordIcon} />
                 </button>
                 <div className={style["role-name"]}>
@@ -26,7 +34,7 @@ export default function SelectRole() {
                 </div>
             </div>
             <div className={style["role-item"]}>
-                <button>
+                <button onClick={() => setRole(4)}>
                     <img src={TenentIcon} />
                 </button>
                 <div className={style["role-name"]}>
