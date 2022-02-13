b3d1c230a767c183a5074f963ca9c77fd1a36fb6
vaichu <vaishnavvaichu123@gmail.com>
2022-02-13 18:27:56 +0530

    loader state control

--- src/components/Loader.tsx
+++ src/components/Loader.tsx
@@ -1,7 +1,22 @@
+import React from "react";
 import { Oval } from "react-loader-spinner";
 import style from './Loader.module.scss';
-export default function Loder() {
-    return <div className={style["loader-container"]}>
-        <Oval secondaryColor="#FFFFFF" strokeWidth={4} color="#08ada7" height={60} width={60} />
-    </div>
+
+
+const setLoaderRef: { current: null | React.Dispatch<React.SetStateAction<boolean>> } = { current: null };
+const setLoader = (enable: boolean) => {
+    setLoaderRef.current && setLoaderRef.current(enable);
 }
+export default function Loder() {
+    const [enabled, setEnabled] = React.useState(false);
+    React.useEffect(() => {
+        setLoaderRef.current = setEnabled;
+    }, [])
+    return <>{enabled &&
+        <div className={style["loader-container"]}>
+            <Oval secondaryColor="#FFFFFF" strokeWidth={4} color="#08ada7" height={60} width={60} />
+        </div>
+    }
+    </>
+}
+export { setLoader }
