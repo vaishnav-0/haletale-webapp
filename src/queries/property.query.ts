713517aebeab93c31ef0e8e28250b0919eda75e0
vaichu <vaishnavvaichu123@gmail.com>
2022-02-17 19:40:39 +0530

    gql

--- src/queries/property.query.ts
+++ src/queries/property.query.ts
@@ -15,14 +15,14 @@ export default {
         }
       }`,
 
-  GET_ALL_PROPERTY_TYPE: `query GET_PROPERTY_TYPE_LIST {
+  GET_ALL_PROPERTY_TYPE: gql`query GET_PROPERTY_TYPE_LIST {
         property_type{
           name,
           description
         }
       }`,
 
-  GET_ALL_PROPERTY_SUBTYPE: `query GET_PROPERTY_SUBTYPE_LIST {
+  GET_ALL_PROPERTY_SUBTYPE: gql`query GET_PROPERTY_SUBTYPE_LIST {
         property_subtype{
           name,
           description
