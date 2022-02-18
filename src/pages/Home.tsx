0c863b007913b668a3ba9ad05e613126e36cc237
vaichu <vaishnavvaichu123@gmail.com>
2022-02-18 20:25:23 +0530

    place suggestion

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -4,22 +4,28 @@ import Layout from './Layout';
 import HomeBanner from '../components/HomeBanner';
 import MinimalPropertyList from '../components/MinimalPropertyList';
 import Searchbar from '../components/Searchbar';
-
-
-// 
-
-//import { createPresignedUrl } from '../functions/image/src/presignedurl'
-// 
-
+import debounce from '../functions/debounce';
 import { geocode, addressToGeo, suggest } from '../functions/api/location'
 
 import { x } from '../functions/image/index'
+
 function HomePage(): JSX.Element {
+    const [searchSuggestion, setSearchSuggestion] = React.useState<string[]>([])
+    const suggestPlaces = (search: string) => {
+        suggest(search).then(data => {
+            setSearchSuggestion(data);
+        }).catch(e => console.log(e))
+    }
+    const debouncedSuggest = React.useCallback(debounce(suggestPlaces, 800), []);
     return (
         <Layout footer={true}>
             <HomeBanner />
             <div style={{ marginTop: 60, marginBottom: 60 }}>
-                <Searchbar />
+                <Searchbar suggestionItems={searchSuggestion.map(e => e[0])}
+                    placeholder="Search Property, Neighbourhood or Address"
+                    onChange={debouncedSuggest}
+                    onSubmit={(v, i) => console.log(v, i)}
+                    submitOnSuggestionClick />
             </div>
             <div style={{ marginTop: 60, marginBottom: 60 }}>
                 <MinimalPropertyList title="Popular properties" />
