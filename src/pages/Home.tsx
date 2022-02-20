d0625400aef9678cccdb6403937c44da3d5f9a75
vaichu <vaishnavvaichu123@gmail.com>
2022-02-20 09:36:55 +0530

    converted to hook

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -4,30 +4,16 @@ import Layout from './Layout';
 import HomeBanner from '../components/HomeBanner';
 import MinimalPropertyList from '../components/MinimalPropertyList';
 import Searchbar from '../components/Searchbar';
-import debounce from '../functions/debounce';
-import { geocode, addressToGeo, suggest } from '../functions/api/location'
-
-import { x } from '../functions/image/index'
-
+import { usePlaceSuggestions } from '../functions/hooks/usePlaceSuggestions';
 function HomePage(): JSX.Element {
-    const [searchSuggestion, setSearchSuggestion] = React.useState<string[]>([])
-    const suggestPlaces = (search: string) => {
-        if (search !== "")
-            suggest(search).then(data => {
-                setSearchSuggestion(data);
-            }).catch(e => console.log(e))
-        else
-            setSearchSuggestion([]);
-
-    }
-    const debouncedSuggest = React.useCallback(debounce(suggestPlaces, 800), []);
+    const { suggestions, suggest } = usePlaceSuggestions();
     return (
         <Layout footer={true}>
             <HomeBanner />
             <div style={{ marginTop: 60, marginBottom: 60 }}>
-                <Searchbar suggestionItems={searchSuggestion.map(e => e[0])}
+                <Searchbar suggestionItems={suggestions.map(e => e[0])}
                     placeholder="Search Property, Neighbourhood or Address"
-                    onChange={debouncedSuggest}
+                    onChange={suggest}
                     onSubmit={(v, i) => console.log(v, i)}
                     submitOnSuggestionClick />
             </div>
