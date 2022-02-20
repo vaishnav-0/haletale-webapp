0025a2cb8bef5ed841cc1deb3227f1fcdf83a279
vaichu <vaishnavvaichu123@gmail.com>
2022-02-21 00:15:15 +0530

    replace functionality

--- src/functions/hooks/useListState.ts
+++ src/functions/hooks/useListState.ts
@@ -1,8 +1,10 @@
 import React from "react";
+
 export type useListStateReturnType<T> = {
     list: any[],
     add: (item: T, pos?: number) => void,
-    remove: (pos?: number) => void
+    remove: (pos?: number) => void,
+    replace: (a: T | [T, number][], pos?: number) => void
 }
 export default function useListState<T>(defaultValue: T[] = []): useListStateReturnType<T> {
     const [list, updateList] = React.useState(defaultValue);
@@ -33,5 +35,32 @@ export default function useListState<T>(defaultValue: T[] = []): useListStateRet
         },
         []
     );
-    return { list, add, remove };
+    const replace: useListStateReturnType<T>['replace'] = React.useCallback(
+        (a, pos) => {
+            if (Array.isArray(a)) {
+                updateList(l => {
+                    let list_ = [...l];
+                    a.forEach((e) => {
+                        if (e[1] < 0)
+                            throw new Error("position shouldn't be negative");
+                        list_.splice(e[1], 1, e[0]);
+                    })
+                    return list_;
+                });
+
+            } else {
+                if (!pos || pos < 0)
+                    throw new Error("position shouldn't be negative");
+                else
+                    updateList(l => {
+                        let list_ = [...l];
+                        list_.splice(pos, 1, a);
+                        return list_;
+                    });
+            }
+
+        },
+        []
+    );
+    return { list, add, remove, replace };
 }
