03937acd97825cdced9c276ff33626220d53ff8a
vaichu <vaishnavvaichu123@gmail.com>
2022-02-21 10:01:38 +0530

    added

---
+++ src/components/Form/components/FormProgressIndicator.tsx
@@ -0,0 +1,37 @@
+import React from "react";
+import style from "./FormProgressIndicator.module.scss";
+import { Oval } from 'react-loader-spinner';
+type PropsType = {
+    height?: number,
+    indicator: string,
+    state: ProgressStateEnum,
+    description: string
+}
+export enum ProgressStateEnum {
+    Pending,
+    OnProgress,
+    Processing,
+    Done
+}
+export default function FormProgressIndicator({ height = 120, indicator, state, description }: PropsType) {
+    return (
+        <div className={style["progress-container"]} style={{ height: height + "px" }}>
+            <div className={style["progress-symbols"]} >
+                <div></div>
+                <button className={`${style["progress-btn"]}${(state === ProgressStateEnum.Done || state === ProgressStateEnum.OnProgress) ? " " + style["active"] : ''}`}>
+                    {state === ProgressStateEnum.Done ?
+                        <i className="fas fa-check" /> :
+                        state === ProgressStateEnum.Processing ?
+                            <Oval secondaryColor="grey" strokeWidth={5} color="white" height={20} width={20} /> :
+                            indicator
+                    }
+                </button>
+                <div></div>
+            </div>
+            <div className={style["progress-description"]}>
+                {description}
+            </div>
+        </div>
+
+    )
+}
