fd2b319dabdd2a78de0a2a625e566fc4cf5f110b
vaichu <vaishnavvaichu123@gmail.com>
2022-02-24 01:37:47 +0530

    query

--- src/pages/PropertySearchListing.tsx
+++ src/pages/PropertySearchListing.tsx
@@ -5,11 +5,36 @@ import style from './PropertySearchListing.module.scss';
 import Layout from './Layout';
 import { RadioButtonGroup } from '../components/Form/components/RadiobuttonGroup';
 import { Openable } from '../components/Openable';
-
+import propertyQuery, { IPropertyDetails } from '../queries/property.query';
+import { useLazyQuery } from '@apollo/client';
+import { useNavigate, useSearchParams } from 'react-router-dom';
 export default function (): JSX.Element {
+    let [getPropertyByDistance, { data: propertyData, loading }] = useLazyQuery<{ show_nearby_properties: IPropertyDetails[] }>(propertyQuery.GET_PROPERTY_BY_DISTANCE)
+    const [searchParams, setSearchParams] = useSearchParams();
+    const navigate = useNavigate();
     const [filterOpen, setFilterOpen] = React.useState(false);
     const sortButtonRef = React.useRef<HTMLButtonElement>(null!);
     const [openSort, setOpenSort] = React.useState(false);
+    console.log(propertyData);
+    React.useEffect(() => {
+        if (searchParams.get("lat") && searchParams.get("lng")) {
+            const coords = [parseFloat(searchParams.get("lat")!),parseFloat(searchParams.get("lng")!)];
+            getPropertyByDistance({
+                variables: {
+                    cur_coords: {
+                        type: "Point",
+                        coordinates: coords
+                    },
+                    distance: 10
+                }
+            });
+
+        } else
+            navigate("/");
+
+    }, [searchParams])
+    React.useEffect(() => {
+    }, [])
     return (
         <Layout>
             <div className={style["header"]}>
