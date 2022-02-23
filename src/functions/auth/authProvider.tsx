7ccf5db8f30702ea33972833a7bdca06d5cf6f0c
vaichu <vaishnavvaichu123@gmail.com>
2022-02-24 03:14:25 +0530

    incorporated user data

--- src/functions/auth/authProvider.tsx
+++ src/functions/auth/authProvider.tsx
@@ -3,9 +3,13 @@ import authContext from './authContext';
 import auth from './index';
 import { toast } from 'react-toastify';
 import { setGlobalLoader } from '../../components/Loader';
-
+import { useLazyQuery } from '@apollo/client';
+import userQuery from '../../queries/user.query';
+import cloneDeep from 'clone-deep';
 export default function AuthProvider({ children }: { children: React.ReactNode }) {
   let [user, setUser] = React.useState<any>(null);
+  let [getUserData, { data: userData, loading: userDataloading }] = useLazyQuery(userQuery.GET_USER_DETAILS);
+
   React.useLayoutEffect(() => {
     if (user === null)
       setGlobalLoader(true, { backgroundColor: "white" })
@@ -15,6 +19,7 @@ export default function AuthProvider({ children }: { children: React.ReactNode }
   React.useEffect(() => {
     return auth.onAuthStateChange(
       (u) => {
+        getUserData({ variables: { id: u?.user_id } });
         console.log(u)
         setUser({ user: u });
       },
@@ -36,9 +41,17 @@ export default function AuthProvider({ children }: { children: React.ReactNode }
           pauseOnHover: true,
           draggable: true,
         });
-        
+
       })
   }, []);
+  React.useEffect(() => {
+    if (userData?.user[0])
+      setUser((u: any) => {
+        const uC = cloneDeep(u)
+        uC.user.user_details = userData.user[0];
+        return uC;
+      })
+  }, [userData])
   return <authContext.Provider value={user}>{children}</authContext.Provider>;
 
 }
