5efc2d79dbf9f5c500c7cca6b589b4da306a920b
vaichu <vaishnavvaichu123@gmail.com>
2022-03-02 17:28:47 +0530

    wrapper

--- src/components/SidebarNavigation.tsx
+++ src/components/SidebarNavigation.tsx
@@ -1,28 +1,32 @@
 import React from "react";
 import { Link } from "react-router-dom";
 import style from './SidebarNavigation.module.scss';
+import Layout from "../pages/Layout";
 export default function SidebarNavigation({ items, children }: { items: { label: string, url: string }[], children: JSX.Element }) {
     const [active, setActive] = React.useState<number>(-1);
     return (
-        <div className={style["sidenav-container"]}>
-            <div className={style["nav-container"]}>
-                <ul className={style["nav-list"]}>
+        <Layout>
+           <div className={style["sidenav-container"]}>
+                <div className={style["nav-container"]}>
+                    <ul className={style["nav-list"]}>
+                        {
+                            items.map((e, i) => <li>
+                                <Link onClick={() => setActive(i)} className={active === i ? style["active"] : ""}
+                                    to={e.url}>{e.label}
+                                </Link>
+                            </li>
+                            )
+                        }
+                    </ul>
+                </div>
+                <div className={style["content"]}>
                     {
-                        items.map((e, i) => <li>
-                            <Link onClick={() => setActive(i)} className={active === i ? style["active"] : ""}
-                                to={e.url}>{e.label}
-                            </Link>
-                        </li>
-                        )
+                        children
                     }
-                </ul>
+                </div>
             </div>
-            <div className={style["content"]}>
-                {
-                    children
-                }
-            </div>
-        </div>
+        </Layout>
+
 
     )
 }
