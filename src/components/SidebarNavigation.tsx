ab4255eac8efb065312e58f16c2217c63c78399d
vaichu <vaishnavvaichu123@gmail.com>
2022-03-02 18:07:59 +0530

    state lift

--- src/components/SidebarNavigation.tsx
+++ src/components/SidebarNavigation.tsx
@@ -2,16 +2,15 @@ import React from "react";
 import { Link } from "react-router-dom";
 import style from './SidebarNavigation.module.scss';
 import Layout from "../pages/Layout";
-export default function SidebarNavigation({ items, children }: { items: { label: string, url: string }[], children: JSX.Element }) {
-    const [active, setActive] = React.useState<number>(-1);
+export default function SidebarNavigation({ items, active, children }: { items: { label: string, url: string }[], children: JSX.Element, active?: number }) {
     return (
         <Layout>
-           <div className={style["sidenav-container"]}>
+            <div className={style["sidenav-container"]}>
                 <div className={style["nav-container"]}>
                     <ul className={style["nav-list"]}>
                         {
                             items.map((e, i) => <li>
-                                <Link onClick={() => setActive(i)} className={active === i ? style["active"] : ""}
+                                <Link className={active === i ? style["active"] : ""}
                                     to={e.url}>{e.label}
                                 </Link>
                             </li>
