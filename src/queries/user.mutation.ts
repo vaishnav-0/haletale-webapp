c5d659edc58ec9213474884a4fa9f1b4216fecc9
Anugeeth <anugeeth2@gmail.com>
2022-03-02 17:53:24 +0530

    queery updates

--- src/queries/user.mutation.ts
+++ src/queries/user.mutation.ts
@@ -8,7 +8,18 @@ export default {
         update_user_role(where: {user_id: {_eq: $id}}, _set: {role_id: $role_id}) {
           affected_rows
         }
-    }`
+    }`,
+
+
+
+  UPDATE_PHONE_COUNTRY:  gql`mutation MyMutation($phone: String, $id: uuid, $nationality: String = "") {
+    update_user(_set: {phone: $phone}, where: {id: {_eq: $id}}) {
+      affected_rows
+    }
+    update_user_detail( _set: {nationality: $nationality},where: {id: {_eq: $id}}){
+      affected_rows
+    }
+  }`,
 }
 
 
