49f6a2a57280be8947a16fbd2c4460222bb3806c
vaichu <vaishnavvaichu123@gmail.com>
2022-03-06 16:39:22 +0530

    new query

--- src/queries/requests.query.ts
+++ src/queries/requests.query.ts
@@ -1,8 +1,8 @@
 import { gql } from "@apollo/client";
 
 
-export default{
-    GET_ALL_REQUESTS : gql`query PROPERTY_REQUEST {
+export default {
+  GET_ALL_REQUESTS: gql`query PROPERTY_REQUEST {
         property_request {
           intended_move_in_date
           lease_duration
@@ -14,5 +14,15 @@ export default{
         }
       }
       `,
+  GET_REQUEST_BY_ID: gql`query PROPERTY_REQUEST($id:uuid) {
+  property_request(where: {tenant_id: {_eq: $id}}) {
+    intended_move_in_date
+    lease_duration
+    living_type
+    other_tenents
+    requested_at
+  }
+}
+`
 
 }
