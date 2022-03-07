aff8a1eb613d23dbf9e28a956ed78e60a8811f6a
vaichu <vaishnavvaichu123@gmail.com>
2022-03-08 01:32:37 +0530

    changed toastcontainer location

--- src/App.tsx
+++ src/App.tsx
@@ -6,12 +6,24 @@ import Router from './routes';
 import Loader from './components/Loader';
 import { ApolloProvider } from '@apollo/client';
 import graphQLClient from './apolloClient';
+import { ToastContainer } from 'react-toastify';
+import 'react-toastify/dist/ReactToastify.css';
 
 function App() {
   const client = graphQLClient();
   return (
     <ApolloProvider client={client}>
       <AuthProvider>
+        <ToastContainer
+          position="bottom-center"
+          autoClose={5000}
+          hideProgressBar={false}
+          newestOnTop={false}
+          rtl={false}
+          pauseOnFocusLoss
+          draggable
+          pauseOnHover
+        />
         <Router />
         <Loader />
       </AuthProvider>
