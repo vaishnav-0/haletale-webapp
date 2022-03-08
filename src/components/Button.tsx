866a28d0d169210d27a77000c1c981769dc660db
vaichu <vaishnavvaichu123@gmail.com>
2022-03-08 10:43:54 +0530

    tooltip

--- src/components/Button.tsx
+++ src/components/Button.tsx
@@ -1,18 +1,22 @@
+import { tooltip } from 'leaflet';
 import React from 'react';
 import style from "./Button.module.scss";
 interface PropsType extends React.DetailedHTMLProps<React.ButtonHTMLAttributes<HTMLButtonElement>, HTMLButtonElement> {
     label?: string;
-    className?: string
+    className?: string;
+    tooltip?: string
 }
 export type ButtonComponentType = React.FC<PropsType>
-export const ButtonHollow: ButtonComponentType = function ({ children, onClick = () => { }, className = "", ...rest }) {
+export const ButtonHollow: ButtonComponentType = function ({ children, onClick = () => { }, className = "", tooltip, ...rest }) {
     return <button onClick={onClick} className={style["btn-smoothround-hollow"] + " " + className} {...rest}>
+        {tooltip && <div className={style["tooltip"]}>{tooltip}</div>}
         {children}
     </button >
 
 }
-export const ButtonSolid: ButtonComponentType = function ({ children, onClick = () => { }, className = "", ...rest }) {
+export const ButtonSolid: ButtonComponentType = function ({ children, onClick = () => { }, className = "", tooltip, ...rest }) {
     return <button onClick={onClick} className={style["btn-smoothround-solid"] + " " + className} {...rest}>
+        {(tooltip || !(tooltip === "")) && <div className={style["tooltip"]}>{tooltip}</div>}
         {children}
     </button>
 
