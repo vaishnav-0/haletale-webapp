d4083b30eb43a112f2c95fcb123689a25f31f318
vaichu <vaishnavvaichu123@gmail.com>
2022-03-10 00:31:35 +0530

    new prop

--- src/routes/RequireAuth.tsx
+++ src/routes/RequireAuth.tsx
@@ -1,7 +1,7 @@
 import React from "react";
-import { Navigate, useLocation, Outlet } from 'react-router-dom';
+import { Navigate, useLocation, Outlet, useNavigate } from 'react-router-dom';
 import { useAuth } from "../functions/auth/useAuth";
-import { Roles } from "../functions/auth/types";
+import { IUser, Roles } from "../functions/auth/types";
 /**
  * @description Redirect user to / if the user is not logged in and opens the login modal
  * @param children 
@@ -9,12 +9,14 @@ import { Roles } from "../functions/auth/types";
  * @returns 
  */
 
-export const RequireAuth = ({ role, redirect = "/" }: { role: Roles[] | [], redirect?: string }) => {
+export const RequireAuth = ({ role, redirect = "/", onReject = () => { } }: { role: Roles[] | [], redirect?: string, onReject?: (user: IUser | null) => void }) => {
   let auth = useAuth();
   let location = useLocation();
+  const navigate = useNavigate();
   if (auth === null)  //auth initializing
     return null;
   if (role.length && (auth.user === null || !role.some(e => auth.user?.role.includes(e)))) {
+    onReject(auth.user);
     return <Navigate to={redirect} state={{ from: location, openLoginModel: true }} replace />;
   } else {
     return <Outlet />;
