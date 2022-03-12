2fd96298599731d6895ba96163716c7e638b514e
vaichu <vaishnavvaichu123@gmail.com>
2022-03-12 21:57:35 +0530

    type modified

--- src/functions/auth/authContext.tsx
+++ src/functions/auth/authContext.tsx
@@ -1,6 +1,6 @@
 import { createContext } from 'react';
 import { AuthContextType } from './types';
 
-const authContext = createContext<AuthContextType>(null!);
+const authContext = createContext<AuthContextType | null>(null);
 
 export default authContext;
--- src/routes/RequireAuth.tsx
+++ src/routes/RequireAuth.tsx
@@ -2,6 +2,7 @@ import React from "react";
 import { Navigate, useLocation, Outlet, useNavigate } from 'react-router-dom';
 import { useAuth } from "../functions/auth/useAuth";
 import { IUser, Roles } from "../functions/auth/types";
+import UserContext from "../functions/auth/userContext";
 /**
  * @description Redirect user to / if the user is not logged in and opens the login modal
  * @param children 
@@ -12,10 +13,9 @@ import { IUser, Roles } from "../functions/auth/types";
 export const RequireAuth = ({ role, redirect = "/", onReject = () => { } }: { role: Roles[] | [], redirect?: string, onReject?: (user: IUser | null) => void }) => {
   let auth = useAuth();
   let location = useLocation();
-  const navigate = useNavigate();
   if (auth === null)  //auth initializing
     return null;
-  if (role.length && (auth.user === null || !role.some(e => auth.user?.role.includes(e)))) {
+  if (role.length && (auth.user === null || !role.some(e => auth!.user?.role.includes(e)))) {
     onReject(auth.user);
     return <Navigate to={redirect} state={{ from: location, openLoginModel: true }} replace />;
   } else {
