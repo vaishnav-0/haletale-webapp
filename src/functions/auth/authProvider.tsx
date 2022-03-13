f17251e8686df5029309721f2043700cb211a02f
vaichu <vaishnavvaichu123@gmail.com>
2022-03-13 13:01:50 +0530

    fix - userDetails not updating

--- src/functions/auth/authProvider.tsx
+++ src/functions/auth/authProvider.tsx
@@ -3,13 +3,17 @@ import authContext from './authContext';
 import auth from './index';
 import { toast } from 'react-toastify';
 import { setGlobalLoader } from '../../components/Loader';
-import { useLazyQuery } from '@apollo/client';
+import { useApolloClient, useLazyQuery } from '@apollo/client';
 import userQuery from '../../queries/user.query';
 import cloneDeep from 'clone-deep';
 import { IUser } from './types';
+
 export default function AuthProvider({ children }: { children: React.ReactNode }) {
-  let [user, setUser] = React.useState<{ user: IUser | null } | null>(null);
-  let [getUserData, { data: userData, loading: userDataloading }] = useLazyQuery(userQuery.GET_USER_DETAILS);
+  const [user, setUser] = React.useState<{ user: IUser | null } | null>(null);
+  const [getUserData, { data: userData, loading: userDataloading, refetch: userDataRefetch, called }] = useLazyQuery(userQuery.GET_USER_DETAILS, {
+    fetchPolicy: "no-cache"
+  });
+  const apollo = useApolloClient();
   React.useLayoutEffect(() => {
     if (user === null)
       setGlobalLoader(true, { backgroundColor: "white" })
@@ -20,24 +24,35 @@ export default function AuthProvider({ children }: { children: React.ReactNode }
     return auth.onAuthStateChange(
       (u) => {
         if (u)
-          getUserData({ variables: { id: u?.user_id } });
-        console.log(u)
-        setUser({ user: u });
+          if (!called)
+            getUserData({ variables: { id: u.user_id } });
+          else
+            userDataRefetch({ id: u.user_id });
+        else
+          apollo.clearStore();
+        console.log(userData?.user[0]?.id);
+        if (u && (userData?.user[0]?.id === u?.user_id)) { //apollo client fetch policy doesn't work as expected
+          const { id, ...uData } = userData.user[0];
+          setUser({ user: { ...u!, user_details: uData } });
+        }
+        else {
+          setUser({ user: u });
+        }
       },
       (err) => {
         console.log(err);
         err?.code === "NotAuthorizedException" && toast.error('Incorrect username or password.');
         err?.code === "NewtworkError" && toast.error('Network error. Please try again.');
       })
-  }, []);
+  }, [called, userData]);
   React.useEffect(() => {
     if (userData?.user[0])
-      setUser((u: any) => {
+      setUser((u) => {
         const uC = cloneDeep(u)
-        uC.user.user_details = userData.user[0];
+        uC!.user!.user_details = userData.user[0];
         return uC;
       })
-  }, [userData]);
+  }, [userData, userDataloading]);
   return <authContext.Provider value={user}>{children}</authContext.Provider>;
 
 }
