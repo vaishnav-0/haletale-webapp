694849bd5f573237673007736839003f6ec32d28
vaichu <vaishnavvaichu123@gmail.com>
2022-03-13 16:36:06 +0530

    new queries and type

--- src/queries/requests.query.ts
+++ src/queries/requests.query.ts
@@ -1,14 +1,48 @@
 import { gql } from "@apollo/client";
 
-
+export interface IRequestData {
+  intended_move_in_date: string,
+  lease_duration: number,
+  other_tenents?:
+  {
+    name?: string
+  }[]
+  ,
+  property_id: string,
+  user: {
+    "email": string,
+    "phone": string,
+    "name": string
+  }
+}
 export default {
+  GET_ALL_REQUEST_COUNT: gql`query GET_ALL_REQUEST_COUNT {
+  property_request_aggregate {
+    aggregate {
+      count
+    }
+  }
+}
+  `,
+  GET_REQUEST_COUNT: gql`query GET_ALL_REQUEST_COUNT($id:uuid) {
+  property_request_aggregate(where: {property_id: {_eq: $id}}) {
+    aggregate {
+      count
+    }
+  }
+}
+  `,
   GET_ALL_REQUESTS: gql`query PROPERTY_REQUEST {
         property_request {
-          intended_move_in_date
-          lease_duration
-          living_type
-          other_tenents
-          requested_at
+        intended_move_in_date
+        lease_duration
+        other_tenents
+        property_id
+        user {
+          email
+          phone
+          name
+        }
         }
       }
       `,
@@ -19,6 +53,11 @@ export default {
     living_type
     other_tenents
     requested_at
+    user {
+          email
+          phone
+          name
+        }
   }
 }
 `
