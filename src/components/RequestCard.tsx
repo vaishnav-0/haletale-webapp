94d552b8dadbfb9b791140b46791c95b1a8aeeac
vaichu <vaishnavvaichu123@gmail.com>
2022-03-13 20:26:29 +0530

    for TenantRequestView compatibility

--- src/components/RequestCard.tsx
+++ src/components/RequestCard.tsx
@@ -15,9 +15,15 @@ export default function RequestCard({ requestData }: { requestData: IRequestData
                         onClick={() => navigate("/property/view?id=" + requestData.property_id)} > {requestData.property_id}</button>
                 </div>
             }
-            <div><span className={style["title"]}>Name: </span>{requestData.user.name}</div>
-            <div><span className={style["title"]}>Email: </span>{requestData.user.email}</div>
-            <div><span className={style["title"]}>Phone: </span>{requestData.user.phone}</div>
+            {
+                requestData.user &&
+                <>
+                    <div><span className={style["title"]}>Name: </span>{requestData.user.name}</div>
+                    <div><span className={style["title"]}>Email: </span>{requestData.user.email}</div>
+                    <div><span className={style["title"]}>Phone: </span>{requestData.user.phone}</div>
+                </>
+            }
+
             <div><span className={style["title"]}>Intended move in date: </span>{requestData.intended_move_in_date}</div>
             <div><span className={style["title"]}>Lease duration: </span> {requestData.lease_duration}</div>
             <div className={style["othertenants"]}>
