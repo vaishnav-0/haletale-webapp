561f9d6ff4fcb2ae4e4319a795fb2927bdf18ce5
vaichu <vaishnavvaichu123@gmail.com>
2022-03-13 13:02:46 +0530

    more genericity

--- src/functions/hooks/useStateWithCB.ts
+++ src/functions/hooks/useStateWithCB.ts
@@ -1,11 +1,10 @@
 import React from 'react';
-export default function <T>(d: T): [T, (v: T, cb?: (v: T) => void) => void] {
+export default function <T>(d: T): [T, (v: React.SetStateAction<T>, cb?: (v: T) => void) => void] {
     const [state, updateState] = React.useState<T>(d);
     const callback = React.useRef<(v: T) => void>(() => { });
-    const update = (val: T, cb?: (v: T) => void) => {
-        let v = (typeof val === 'function') ? val(state) : val;
+    const update = (val: React.SetStateAction<T>, cb?: (v: T) => void) => {
         cb && (callback.current = cb);
-        updateState(val)
+        updateState(val);
     }
     React.useEffect(() => {
         if (typeof callback.current === 'function')
