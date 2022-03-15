8157009c95fd4a4fddcc2a5eee8f9d153cf4c9ee
vaichu <vaishnavvaichu123@gmail.com>
2022-03-15 23:20:39 +0530

    added data flow

--- src/components/MapViewTile.tsx
+++ src/components/MapViewTile.tsx
@@ -5,35 +5,52 @@ import propertyImg1 from "../assets/images/property.jpeg";
 import propertyImg2 from "../assets/images/housewide.jpg";
 import bathIcon from '../assets/icons/bath_white.svg';
 import bedIcon from '../assets/icons/bed_white.svg';
-
-const MapViewTile = React.forwardRef<HTMLDivElement, React.DetailedHTMLProps<React.HTMLAttributes<HTMLDivElement>, HTMLDivElement>>((props, ref) => {
-    const [fav, setFav] = React.useState(false);
-    return <div ref={ref} className={style["item"]} {...props}>
+import { Link } from "react-router-dom";
+import defaultImage from '../assets/images/property_placeholder.jpg'
+import { IPropertyDetails } from "../queries/property.query";
+import ClampLines from "react-clamp-lines";
+import useFavourite, { FavButton } from "../functions/hooks/useFavourite";
+interface PropsType {
+    highlight?: boolean,
+    property: IPropertyDetails,
+    onClick?: () => void
+}
+const MapViewTile = React.forwardRef<HTMLDivElement, PropsType>((props, ref) => {
+    const { property, highlight } = props;
+    const favControls = useFavourite(property.id!)
+    const images = property.property_images?.map(e => e?.s3Url?.url ?? "").slice(0, 1);
+    return <div ref={ref} className={style["item"] + ` ${highlight ? style["highlight"] : ""}`} onClick={props.onClick}>
         <div className={style["item-top-container"]}>
-            <div className={style["rate"]}>C$ 120 Month</div>
-            <button onClick={(e) => { setFav(!fav); e.stopPropagation() }} className={style["fav-icon"]}>
-                <i
-                    className={`${fav ? style.filled + " fas" : style.regular + " far"} fa-heart`}
-                ></i>
-            </button>
+            <div className={style["rate"]}>C$ {property.property_detail?.rent_amount}</div>
+            <div className={style["fav-icon-container"]}>
+                <FavButton control={favControls} />
+            </div>
             <div className={style["property-feature"]}>
                 <div>
                     <img src={bedIcon} alt='' />
-                    3 bed
+                    {property.property_detail?.rooms?.bedroom} bed
                 </div>
                 <div className={style["seperator"]}></div>
                 <div>
                     <img src={bathIcon} alt='' />
-                    2 bath
+                    {property.property_detail?.rooms?.bathroom} bath
                 </div>
             </div>
         </div>
-        <div className={style["property-location"]}>
-            <div>Town House</div>
-
-            <div>130 Clinton Street - Toronto, ON</div>
+        <div className={style["item-bottom-container"]}>
+            <div className={style["property-location"]}>
+                <div>{property.type}</div>
+                <ClampLines
+                    text={property.property_address?.address?.full_address ?? ""}
+                    id={Math.random() * 100000 + (property.id ?? "")}
+                    lines={2}
+                    stopPropagation={true}
+                    buttons={false}
+                />
+            </div>
+            <Link to={"/property/view/?id=" + property.id}><i className="fas fa-arrow-right" /></Link>
         </div>
-        <ImageSlider imgSrc={[propertyImg1, propertyImg2]} aspectRatio={16 / 9} indicatorClassName={style["position-indicator"]}
+        <ImageSlider imgSrc={!images || images.length === 0 ? [defaultImage] : images} aspectRatio={16 / 9} indicatorClassName={style["position-indicator"]}
             showIndicators={false} swipable={false}
         />
     </div>
