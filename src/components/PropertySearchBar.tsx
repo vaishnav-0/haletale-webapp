83cb2ef664a5f1cabd6106905abea6b8fc62e9bc
vaichu <vaishnavvaichu123@gmail.com>
2022-03-17 09:58:39 +0530

    params for search

--- src/components/PropertySearchBar.tsx
+++ src/components/PropertySearchBar.tsx
@@ -5,6 +5,7 @@ import Searchbar from '../components/Searchbar';
 import { toast } from 'react-toastify';
 import { setGlobalLoader } from '../components/Loader';
 import { addressToGeo } from '../functions/api/location';
+import { objectFilter } from '../functions/utils';
 
 export default function (): JSX.Element {
     const { suggestions, suggest } = usePlaceSuggestions();
@@ -13,9 +14,27 @@ export default function (): JSX.Element {
         setGlobalLoader(true);
         addressToGeo(placeId).then(d => {
             setGlobalLoader(false)
+            const addressComponents = ["administrative_area_level_1", "administrative_area_level_2", "country", "locality", "route", "street_number", "postal_code"] as const;
+            let addressComponentValues;
+            addressComponentValues = d.address_components.reduce((obj: any, curr: any) => {
+                const type = curr.types.find((e: any) => addressComponents.includes(e))
+                if (type)
+                    obj[type] = curr.long_name;
+                return obj;
+            }, {});
+            console.log(addressComponentValues);
             navigate({
                 pathname: "/properties",
-                search: `?${createSearchParams({ lat: d.geometry.location.lat, lng: d.geometry.location.lng, place: place })}`
+                search: `?${createSearchParams(objectFilter({
+                    c: addressComponentValues.country,
+                    loc: addressComponentValues.locality,
+                    postal: addressComponentValues.postal_code,
+                    route: addressComponentValues.route,
+                    sn: addressComponentValues.street_number,
+                    a1: addressComponentValues.administrative_area_level_1,
+                    a2: addressComponentValues.administrative_area_level_2,
+                    place: place
+                }, (k, v) => !!v))}`
             })
         }).catch(() => {
             setGlobalLoader(false)
@@ -23,12 +42,12 @@ export default function (): JSX.Element {
         })
     }
     return (
-            <div style={{ marginTop: 60, marginBottom: 60 }}>
-                <Searchbar suggestionItems={suggestions.map(e => e[0])}
-                    placeholder="Search Property, Neighbourhood or Address"
-                    onChange={suggest}
-                    onSubmit={(v, i) => searchProperty(suggestions[i!][1], v)}
-                    submitOnSuggestionClick />
-            </div> 
+        <div style={{ marginTop: 60, marginBottom: 60 }}>
+            <Searchbar suggestionItems={suggestions.map(e => e[0])}
+                placeholder="Search Property, Neighbourhood or Address"
+                onChange={suggest}
+                onSubmit={(v, i) => searchProperty(suggestions[i!][1], v)}
+                submitOnSuggestionClick />
+        </div>
     );
 }
