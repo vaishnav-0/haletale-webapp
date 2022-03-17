2064586592888504c5d907c7e19b1f8bae1dc1ce
vaichu <vaishnavvaichu123@gmail.com>
2022-03-17 17:20:58 +0530

    order country

--- src/queries/meta.query.ts
+++ src/queries/meta.query.ts
@@ -2,13 +2,13 @@
 import { gql } from '@apollo/client'
 
 export default {
-    GET_COUNTRIES: gql`query GET_COUNTRIES {
-        countries {
+  GET_COUNTRIES: gql`query GET_COUNTRIES {
+        countries(order_by: {isoCode: asc}) {
           dialCode
           isoCode
           name
           id
         }
       }
-      `
+      `,
 }
