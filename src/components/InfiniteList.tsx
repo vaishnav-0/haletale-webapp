811896f8bf5ec00712fbdabfabc5e5d90e545800
vaichu <vaishnavvaichu123@gmail.com>
2022-03-17 18:18:02 +0530

    seperate aggregate query for efficiency

--- src/components/InfiniteList.tsx
+++ src/components/InfiniteList.tsx
@@ -3,20 +3,23 @@ import { useLazyQuery, DocumentNode, TypedDocumentNode, QueryLazyOptions, Operat
 import InView from 'react-intersection-observer';
 import { useLoder } from './Loader';
 //Infinite scroll using offset. Query must contain variable named offset.
-export default function <T>({ query, initialParams, children, wrapperClassName, checkSkip }:
+export default function <TDataQuery, TAggrQuery>({ query, initialParams, children, wrapperClassName, checkSkip, aggregateQuery }:
     {
-        query: DocumentNode | TypedDocumentNode<T>,
+        query: DocumentNode | TypedDocumentNode<TDataQuery>,
         initialParams: QueryLazyOptions<OperationVariables> | undefined,
-        children: (data: T | undefined, loading: boolean) => JSX.Element | JSX.Element[],
+        children: (data: TDataQuery | undefined, loading: boolean) => JSX.Element | JSX.Element[],
         wrapperClassName?: string,
-        checkSkip: (data: T | undefined) => boolean
+        checkSkip: (data: TDataQuery | undefined, aData: TAggrQuery | undefined) => boolean,
+        aggregateQuery: DocumentNode | TypedDocumentNode<TAggrQuery>
 
     }): JSX.Element {
-    let [lazyQuery, { data, loading, fetchMore, called }] = useLazyQuery<T>(query, {
+    const [lazyQuery, { data, loading, fetchMore, called }] = useLazyQuery<TDataQuery>(query, {
         notifyOnNetworkStatusChange: true
     });
+    const [lazyAggrQuery, { data: aggData, loading: aggLoading }] = useLazyQuery<TAggrQuery>(aggregateQuery);
     const [Loader, setLoader] = useLoder({});
     React.useEffect(() => {
+        lazyAggrQuery(initialParams);
         lazyQuery(initialParams);
     }, [initialParams]);
     React.useEffect(() => {
@@ -25,7 +28,6 @@ export default function <T>({ query, initialParams, children, wrapperClassName, 
         else
             setLoader(false);
     }, [called, loading])
-    console.log(loading)
     return (
         <div className={wrapperClassName}>
             {
@@ -42,7 +44,7 @@ export default function <T>({ query, initialParams, children, wrapperClassName, 
                         })
                     }
                 }}
-                skip={checkSkip(data)}
+                skip={checkSkip(data, aggData)}
             />
         </div>
     );
