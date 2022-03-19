cd6f0b47f6b6d6c845238b6e544066f1af12cef3
vaichu <vaishnavvaichu123@gmail.com>
2022-03-19 14:51:44 +0530

    css module, clicking animation

--- src/components/HomeBanner.tsx
+++ src/components/HomeBanner.tsx
@@ -1,18 +1,24 @@
 import React from 'react';
-import "./HomeBanner.scss";
+import style from "./HomeBanner.module.scss";
 import bannerImg from "../assets/images/BG.jpg"
 export default function (): JSX.Element {
     return (
-        <div className="homebanner-container">
-            <div className="overlay">
-                <div className="banner-subtext">
-                    Sed ut perspiciatis unde omnis iste natus error sit voluptatem accusantium doloremque laudantium
-                </div>
-                <div className="banner-maintext">
-                    Renting house for newcomers is just a few clicks away
+        <div className={style["homebanner-container"]} >
+            <div className={style["overlay"]}>
+                {
+                    //                    <div className={style["banner-subtext"]}>
+                    //                    Sed ut perspiciatis unde omnis iste natus error sit voluptatem accusantium doloremque laudantium
+                    //                </div>
+                }
+                <div className={style["banner-maintext"]}>
+                    Renting house for newcomers is just a few
+                    <span className={style["clicking-anim"]}>
+                        <i className="far fa-hand-point-up"></i>
+                    </span>
+                    away
                 </div>
 
             </div>
-        </div>
+        </ div>
     );
 }
