75eed93683d723eb5e53c93ee12d5fd369623134
vaichu <vaishnavvaichu123@gmail.com>
2022-03-20 12:56:28 +0530

    new component

--- src/components/PopupDialog.tsx
+++ src/components/PopupDialog.tsx
@@ -5,22 +5,42 @@ import style from './PopupDialog.module.scss';
 type PropsType = {
     message: string,
     onConfirm: () => void,
-    onReject: () => void
+    onReject: () => void,
+    openState: [boolean, React.Dispatch<React.SetStateAction<boolean>>]
 }
 export default function PopupDialog(props: PropsType) {
-    const openState = React.useState<boolean>(false);
-    return <Openable open={openState}>
-        <div>
-            <button className={style["btn-close"]}><i className="fas fa-times"></i></button>
+    return <Openable open={props.openState}>
+        <div className={style["container"]} >
+            <button onClick={() => props.openState[1](false)} className={style["btn-close"]}><i className="fas fa-times"></i></button>
             <div className={style["message"]}>
                 {
                     props.message
                 }
             </div>
             <div className={style["btn-container"]}>
-                <button onClick={props.onConfirm} className={style["btn-conf"]}><i className="fas fa-check"></i></button>
-                <button onClick={props.onReject} className={style["btn-rej"]}><i className="fas fa-times"></i></button>
+                <button onClick={() => { props.onConfirm(); props.openState[1](false) }} className={style["btn-conf"]}><i className="fas fa-check"></i></button>
+                <button onClick={() => { props.onReject(); props.openState[1](false) }} className={style["btn-rej"]}><i className="fas fa-ban"></i></button>
             </div>
         </div>
     </Openable>
+}
+
+export function usePopupDialog(): [JSX.Element, (state: boolean, message: string, onConfirm?: (() => void) | undefined, onReject?: (() => void) | undefined) => void] {
+    const [open, setOpen] = React.useState<boolean>(false);
+    const onConfirmRef = React.useRef(() => { });
+    const onRejectRef = React.useRef(() => { });
+    const messageRef = React.useRef("");
+    const expFn = React.useCallback((state: boolean, message: string, onConfirm?: () => void, onReject?: () => void) => {
+        if (!state) {
+            setOpen(state)
+            return;
+        }
+        messageRef.current = message;
+        onConfirmRef.current = onConfirm ?? (() => { });
+        onRejectRef.current = onReject ?? (() => { });
+        setOpen(state);
+    }, [])
+    return [<PopupDialog openState={[open, setOpen]} message={messageRef.current} onConfirm={onConfirmRef.current} onReject={onRejectRef.current} />,
+        expFn
+    ]
 }
