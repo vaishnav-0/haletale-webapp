f95db1e1b60a7a217df0ce6c6d08a0de1b543616
vaichu <vaishnavvaichu123@gmail.com>
2022-03-20 18:05:43 +0530

    move

--- src/pages/Tenant/Dashboard/TenantRequestView.tsx
+++ src/pages/Tenant/Dashboard/TenantRequestView.tsx
@@ -1,19 +1,27 @@
 import { useQuery } from "@apollo/client";
-import requestsQuery, { IRequestData } from "../../queries/requests.query";
-import Layout from "../Layout";
-import RequestCard from "../../components/RequestCard";
-import landLordDashboardStyle from '../Landlord/LandlordDashboard.module.scss';
+import requestsQuery, { IRequestData } from "../../../queries/requests.query";
+import RequestCard from "../../../components/RequestCard";
+import landLordDashboardStyle from '../../Landlord/LandlordDashboard.module.scss';
+import CenterContent from "../../../components/CenterContent";
 
 export default function () {
     const { data: requestData, loading, fetchMore } = useQuery<IRequestData>(requestsQuery.GET_REQUESTED_PROPERTIES);
-    return <Layout>
+    return <>
         <div className={landLordDashboardStyle["viewrequest-heading"]}>
             My bookings:
         </div>
+        {
+            requestData?.property_request.length === 0 ?
+                <CenterContent>
+                    No requests
+                </CenterContent>
+                :
+                null
+        }
         <div className={landLordDashboardStyle["viewrequest-wrapper"]}>
             {
                 requestData?.property_request.map(request => <RequestCard requestData={request} />)
             }
         </div>
-    </Layout>
+    </>
 }
