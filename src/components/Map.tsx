321b66f5102b9b473b88e3ce1f1bfac32ba9aaff
vaichu <vaishnavvaichu123@gmail.com>
2022-03-21 14:24:19 +0530

    marker position fix

--- src/components/Map.tsx
+++ src/components/Map.tsx
@@ -3,7 +3,7 @@ import { MapContainer, TileLayer, Marker, Popup, MapContainerProps } from 'react
 import { Map as LeafletMap } from 'leaflet';
 import L from 'leaflet';
 import 'leaflet/dist/leaflet.css';
-import pinIcon from '../assets/icons/map_pin.svg';
+import pinIcon from '../assets/icons/mapPin.svg';
 import style from './Map.module.scss';
 import mapBoxLogo from '../assets/icons/mapbox-logo-black.png';
 
@@ -11,13 +11,15 @@ export type MarkerType = { onClick: () => void, coords: [number, number], highli
 const iconPin = new L.Icon({
     iconUrl: pinIcon,
     iconRetinaUrl: pinIcon,
-    iconSize: new L.Point(60, 75),
-    className: style['location-pin']
+    iconSize: new L.Point(20, 26),
+    className: style['location-pin'],
+    iconAnchor: [10, 26]
 });
 const iconPinHighlight = new L.Icon({
     iconUrl: pinIcon,
     iconRetinaUrl: pinIcon,
-    iconSize: new L.Point(80, 95),
+    iconSize: new L.Point(26, 33),
+    iconAnchor: [13, 33],
     className: style['location-pin-highlight']
 });
 interface propType extends MapContainerProps {
