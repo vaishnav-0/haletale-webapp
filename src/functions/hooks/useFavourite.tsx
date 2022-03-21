ffe8302c2ec06fe7424018e2604d6bb79d8922d1
vaichu <vaishnavvaichu123@gmail.com>
2022-03-21 13:42:58 +0530

    skip prop

--- src/functions/hooks/useFavourite.tsx
+++ src/functions/hooks/useFavourite.tsx
@@ -1,5 +1,5 @@
 import React from 'react';
-import { useMutation, useQuery } from '@apollo/client';
+import { useLazyQuery, useMutation } from '@apollo/client';
 import userQuery, { IGetUserFav } from '../../queries/user.query';
 import { userMutation } from '../../queries';
 import style from './useFavourite.module.scss'
@@ -7,13 +7,9 @@ import { useAuth } from '../auth/useAuth';
 import { toast } from 'react-toastify';
 
 type TUseFavReturn = [boolean | null, () => void, boolean] //fav state, change fav state, updating state]
-export default function (propertyId: string): TUseFavReturn {
+export default function (propertyId: string, skip: boolean = false): TUseFavReturn {
     const [fav, setFav] = React.useState<boolean | null>(null);
-    const { data: favCheck, loading: favCheckLoading, error: favCheckError, refetch: favRecheck } = useQuery<IGetUserFav>(userQuery.CHECK_USER_FAV, {
-        variables: {
-            property_id: propertyId
-        }
-    });
+    const [getFav, { data: favCheck, loading: favCheckLoading, error: favCheckError, refetch: favRecheck }] = useLazyQuery<IGetUserFav>(userQuery.CHECK_USER_FAV);
     const auth = useAuth();
     const [delFavMutation, { data: delFavData, loading: delFavLoading, error: delFavError }] = useMutation(userMutation.DELETE_FAV);
     const [insertFavMutation, { data: insertFavData, loading: insertFavLoading, error: insertFavError }] = useMutation(userMutation.ADD_USER_FAV);
@@ -21,6 +17,14 @@ export default function (propertyId: string): TUseFavReturn {
         if (Array.isArray(favCheck?.user_favourites))
             setFav(!!favCheck!.user_favourites.length);
     }, [favCheck]);
+    React.useEffect(() => {
+        if (!skip)
+            getFav({
+                variables: {
+                    property_id: propertyId
+                }
+            })
+    }, [skip])
     const changeFavourite = () => {
         if (!auth?.user) {
             toast.warn("Sign in to add this to favourites");
@@ -48,12 +52,16 @@ export default function (propertyId: string): TUseFavReturn {
     return [fav, changeFavourite, delFavLoading || insertFavLoading];
 }
 
-export function FavButton(props: { control: TUseFavReturn }) {
+export function FavButton(props: { control: TUseFavReturn, hide?: boolean }) {
     const [fav, changeFav, favUpdating] = props.control;
     return (
-        <button className={style["heart-btn"]}>
-            <i onClick={changeFav}
-                className={`${fav ? style["heartfilled"] + " fas" : " far"} fa-heart ${favUpdating ? style["heart-loading"] : ""}`}></i>
-        </button>
+        <>{
+            !props.hide &&
+            <button className={style["heart-btn"]}>
+                <i onClick={changeFav}
+                    className={`${fav ? style["heartfilled"] + " fas" : " far"} fa-heart ${favUpdating ? style["heart-loading"] : ""}`}></i>
+            </button>
+        }
+        </>
     );
 }
