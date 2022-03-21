c5a2c74c407ebf4557c3e1551bfc27ea5ccc5b96
vaichu <vaishnavvaichu123@gmail.com>
2022-03-21 18:08:31 +0530

    completed

--- src/pages/Landlord/EditProperty.tsx
+++ src/pages/Landlord/EditProperty.tsx
@@ -1,20 +1,35 @@
 import React from "react";
 import { useNavigate, useSearchParams } from "react-router-dom";
-import FormGenerator, { SchemaType } from "../../components/Form/FormGenerator";
+import FormGenerator, { SchemaType, FormDataShape } from "../../components/Form/FormGenerator";
 import { dataMapReturn, dynamicSchemaGenerator } from "../../components/Form/FormGeneratorHelpers";
 import Layout from "../Layout";
 import formStyle from '../../components/Form/Form.module.scss';
-import { useLazyQuery, useQuery } from "@apollo/client";
-import propertyQuery, { IPropertyDetails } from "../../queries/property.query";
+import { useLazyQuery, useMutation, useQuery } from "@apollo/client";
+import propertyQuery, { IPropertyAttribute, IPropertyDetails } from "../../queries/property.query";
 import { defaultValueInjector } from "../../components/Form/FormGeneratorHelpers";
 import { toast } from "react-toastify";
 import * as yup from 'yup';
 import { useLoader } from "../../components/Loader";
+import propertyMutation from "../../queries/property.mutation";
+import { objectFilter } from "../../functions/utils";
+import cloneDeep from "clone-deep";
+import { cropToAspectRatio } from "../../components/Form/components/Images";
+import { handleImage } from "../../functions/api/imageUpload";
+import { imageMutation } from "../../queries";
 
 const stringFieldRequired = yup.string().required("This field is required.")
-const schema: SchemaType = {
+const schema = {
     heading: "Edit Property",
     items: [
+        {
+            title: "Property name",
+            name: "property_name",
+            type: "text",
+            props: {
+                type: "text",
+                disabled: true
+            },
+        },
         {
             title: "Change address",
             name: "address",
@@ -120,7 +135,7 @@ const schema: SchemaType = {
             props: {
                 values: { "": "", "snow": "6 Months to 1 year", "lawn": "1 year" }
             },
-            validationSchema: stringFieldRequired
+            validationSchema: yup.number().positive("Lease term is required")
         },
         {
             title: "Rent",
@@ -137,14 +152,23 @@ const schema: SchemaType = {
     ],
     submitButton: "Next",
 
-}
+} as const;
+type TFormData = FormDataShape<typeof schema>
 export default function EditProperty() {
-    const [searchParams, setSearchParams] = useSearchParams();
     const [_schema, _setSchema] = React.useState<SchemaType | null>(null);
     const [Loader, setLoader] = useLoader({});
-    let { data: property_types, loading } = useQuery(propertyQuery.GET_ALL_PROPERTY_TYPE_SUBTYPE);
-    let [getProperty, { data: propertyData, loading: propertyloading, error }] = useLazyQuery<{ property: IPropertyDetails[] }>(propertyQuery.GET_PROPERTY_BY_ID, { fetchPolicy: "network-only" });
+    const [searchParams, setSearchParams] = useSearchParams();
     const navigate = useNavigate();
+
+    const [updateProperty, { data: updatePropertyData, loading: updatePropertyLoading, error: updatePropertyError }] = useMutation(propertyMutation.UPDATE_PROPERTY);
+    const [deleteImages, { data: deleteImagesData, loading: deleteImagesLoading, error: deleteImagesError }] = useMutation(imageMutation.DELETE_IMAGES);
+    const [addImages, { data: addImagesData, loading: addImagesLoading, error: addImagesError }] = useMutation(propertyMutation.ADD_PROPERTY_IMAGES);
+
+    const [getProperty, { data: propertyData, loading: propertyloading, error }] = useLazyQuery<{ property: IPropertyDetails[] }>(propertyQuery.GET_PROPERTY_BY_ID, { fetchPolicy: "network-only" });
+    const { data: property_attributes, loading: propertyAttributesLoading } = useQuery<IPropertyAttribute>(propertyQuery.PROPERTY_ATTRIBUTES);
+    const { data: property_types, loading: propertyTypesLoading } = useQuery(propertyQuery.GET_ALL_PROPERTY_TYPE_SUBTYPE);
+    const property = propertyData?.property[0]
+
     React.useEffect(() => {
         if (!searchParams.get("id"))
             navigate("/")
@@ -157,39 +181,44 @@ export default function EditProperty() {
         }
     }, []);
     React.useEffect(() => {
-        if (loading)
+        if (propertyloading || propertyTypesLoading || updatePropertyLoading || deleteImagesLoading || addImagesLoading || propertyAttributesLoading)
             setLoader(true);
-
-    }, [loading]);
+    }, [propertyloading, propertyTypesLoading, updatePropertyLoading, deleteImagesLoading, addImagesLoading, propertyAttributesLoading]);
     React.useEffect(() => {
-        if (propertyData) {
-            console.log(propertyData);
+        if (updatePropertyData && addImagesData && deleteImagesData) {
+            setLoader(false);
+            navigate("/landlord/dashboard");
+        }
+    }, [updatePropertyData, addImagesData, deleteImagesData])
+    React.useEffect(() => {
+        if (propertyData && property_attributes && property_types) {
             const property = propertyData.property[0];
             const defaultValue = {
-                property_address: property.property_address?.address?.full_address,
-                // property_coords: property.coordinates?.coordinates,
+                property_name: property.name,
                 type: property.property_type.id,
                 subtype: property.property_subtype.id,
-                description: property.description,
+                notes: property.description,
                 bedroom: property.property_detail?.rooms?.bedroom,
                 bathroom: property.property_detail?.rooms?.bathroom,
                 parking: property.property_detail?.rooms?.parking,
                 features: property.property_detail?.features ?? [],
                 restriction: property.property_detail?.restrictions ?? [],
-                rent: property.property_detail?.rent_amount
+                rent: property.property_detail?.rent_amount,
+                lease_term: property.property_detail?.lease_term
             }
-            defaultValueInjector(schema, defaultValue).then(s => {
-                console.log("set schema", s);
+            defaultValueInjector(schema as SchemaType, defaultValue).then(s => {
                 dynamicSchemaGenerator({
                     schema: s,
                     dataLoader: {
                         type: property_types.property_type.map((e: any) => { return { [e.id]: e.name } }),
                         subtype: property_types.property_subtype.map((e: any) => { return { [e.id]: e.name } }),
+                        features: property_attributes?.property_features_list.map((e: any) => e.name),
+                        restriction: property_attributes?.property_restrictions_list.map((e: any) => e.name),
+                        lease_term: property_attributes?.lease_term_list,
                     },
                     dataMap: (data) => {
                         return {
                             images: (item: any) => {
-                                console.log(property.property_images?.map(e => { return { url: e?.s3Url, name: e?.key } }))
                                 item.props.fetchList = property.property_images ? property.property_images.map(e => { return { url: e?.s3Url?.url, name: e?.key } }) : []
                             },
                             address: (item: any) => {
@@ -213,6 +242,16 @@ export default function EditProperty() {
                                     }, {})
                                 };
                             },
+                            features: (item: any) => { item.props.items = data.features },
+                            restriction: (item: any) => { item.props.values = data.restriction },
+                            lease_term: (item: any) => {
+                                item.props.values = {
+                                    "0": "", ...data.lease_term.reduce((obj: any, curr: any) => {
+                                        obj = { ...obj, ...{ [curr.id]: curr.description } }
+                                        return obj;
+                                    }, {})
+                                }
+                            }
                         }
                     }
                 }).then(sch => {
@@ -229,9 +268,86 @@ export default function EditProperty() {
         if (error)
             navigate("/");
     }, [error])
+    const onSubmit = (d: TFormData) => {
+        // image updation
+        const imagesToDelete = cloneDeep(property?.property_images?.map(e => e?.key));
+        const imagesToOmit = [];
+        const imgKeys = d.images.map((e: any) => e.file.name);
+        for (let i = 0, deleted = 0; i < imgKeys.length; i++) {
+            let pos = property?.property_images?.findIndex(e => e?.key === imgKeys[i]);
+            if (pos !== -1 && pos !== undefined) {
+                imagesToDelete?.splice(pos - deleted, 1);
+                deleted++;
+                imagesToOmit.push(i);
+            }
+        }
+        while (imagesToOmit.length) {
+            d.images.splice(imagesToOmit.pop(), 1);
+        }
+        cropToAspectRatio(d.images, 16 / 9).then(async t => {
+            try {
+                let keys = await handleImage(d.images);
+                const imageVariable = keys.map((x: string) => {
+                    return {
+                        key: x,
+                        property_id: property?.id
+                    }
+                });
+
+                addImages({
+                    variables: {
+                        object: imageVariable
+                    }
+                })
+
+            } catch (e) {
+                console.log(e);
+            }
+        }).catch(e => {
+            console.log(e)
+        })
+        deleteImages({
+            variables: {
+                keys: imagesToDelete
+            }
+        })
+        console.log(imagesToDelete, imagesToOmit);
+
+
+        let propertyUpdateData = {
+            ...d.address ? {
+                coordinates: {
+                    "type": "Point",
+                    "coordinates": d.address.coords
+                }
+            } : {},
+            description: d.notes,
+            type: d.type,
+            sub_type: d.subtype
+        }
+        let details = {
+            features: d.features,
+            max_occupants: d.tenant_count,
+            rent_amount: d.rent,
+            restrictions: d.restriction,
+            lease_term: d.lease_term,
+            rooms: { bedroom: d.bedroom, bathroom: d.bathroom, parking: d.parking },
+        }
+        updateProperty({
+            variables: {
+                id: property?.id,
+                property: propertyUpdateData,//objectFilter(property, (k, v) => v !== undefined),
+                details: details,
+                address: d.address?.addressComponents,
+                address_id: property?.property_address?.address?.id,
+
+            }
+        })
+    }
     return <Layout>
+        {Loader}
         {_schema &&
-            <FormGenerator schema={_schema} onSubmit={(d) => console.log(d)} />
+            <FormGenerator schema={_schema} onSubmit={onSubmit} />
         }
     </Layout>
 }
