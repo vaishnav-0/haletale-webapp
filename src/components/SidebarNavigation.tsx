c223c488bc18ea443fd17437f0f2e59d1dcf51b4
vaichu <vaishnavvaichu123@gmail.com>
2022-03-29 21:41:56 +0530

    responsiveness

--- src/components/SidebarNavigation.tsx
+++ src/components/SidebarNavigation.tsx
@@ -1,12 +1,37 @@
 import React from "react";
 import { Link } from "react-router-dom";
 import style from './SidebarNavigation.module.scss';
-import Layout from "../pages/Layout";
 export default function SidebarNavigation({ items, active, children }: { items: { label: string, url: string }[], children: JSX.Element, active?: number }) {
+    const sidebarRef = React.useRef<HTMLDivElement>(null);
+    const childRef = React.useRef<HTMLUListElement>(null);
+    const [overflow, setOverflow] = React.useState<boolean>(false);
+    const overFlowRef = React.useRef<boolean>(false);
+    overFlowRef.current = overflow;
+    React.useEffect(() => {
+        if (sidebarRef.current) {
+            function _handleResize() {
+                if (sidebarRef.current!.offsetParent !== null) {
+                    if (!overFlowRef.current)
+                        if (sidebarRef.current!.scrollWidth > sidebarRef.current!.clientWidth)
+                            setOverflow(true)
+                        else
+                            setOverflow(false)
+                }
+            }
+            const ro = new ResizeObserver(_handleResize)
+            ro.observe((sidebarRef.current));
+
+
+            // Cleanup
+            return () => {
+                ro.disconnect()
+            }
+        }
+    }, [sidebarRef])
     return (
-        <div className={style["sidenav-container"]}>
-            <div className={style["nav-container"]}>
-                <ul className={style["nav-list"]}>
+        <div className={`${style["sidenav-container"]} ${overflow ? style["overflow"] : ""}`}>
+            <div ref={sidebarRef} className={style["nav-container"]}>
+                <ul ref={childRef} className={style["nav-list"]}>
                     {
                         items.map((e, i) => <li key={i}>
                             <Link className={active === i ? style["active"] : ""}
