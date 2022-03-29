d21a7761d72905f31b475ee22d9c431fa32f87ea
vaichu <vaishnavvaichu123@gmail.com>
2022-03-29 17:49:01 +0530

    refactored

--- src/pages/Admin/Dashboard/Properties.tsx
+++ src/pages/Admin/Dashboard/Properties.tsx
@@ -1,5 +1,4 @@
 import React from 'react';
-import { useTable, usePagination } from 'react-table'
 import style from './styles.module.scss';
 import { gql, useMutation, useQuery } from '@apollo/client'
 import propertyQuery, { IGetAllPropertyData, IPropertyDetails } from '../../../queries/property.query';
@@ -7,6 +6,7 @@ import { useNavigate } from 'react-router-dom';
 import ImageSlider from '../../../components/ImageSlider';
 import propertyMutation from '../../../queries/property.mutation';
 import { useLoader } from '../../../components/Loader';
+import Table from '../../../components/Table';
 
 export default function Properties() {
   const navigate = useNavigate();
@@ -105,113 +105,7 @@ export default function Properties() {
 // table fn reusabl..
 
 
-function Table({ columns, data }: { columns: any, data: any }) {
-  const {
 
-    getTableProps,
-    getTableBodyProps,
-    headerGroups,
-    prepareRow,
-    page,
-    canPreviousPage,
-    canNextPage,
-    pageOptions,
-    pageCount,
-    gotoPage,
-    nextPage,
-    previousPage,
-    setPageSize,
-    state: { pageIndex, pageSize },
-  } = useTable(
-    {
-      columns,
-      data,
-      initialState: { pageIndex: 0 },
-    },
-    usePagination
-  )
-
-  return (
-    <>
-      <table {...getTableProps()} className={style["table"]}>
-        <thead>
-          {headerGroups.map(headerGroup => (
-            <tr {...headerGroup.getHeaderGroupProps()}>
-              {headerGroup.headers.map(column => (
-                <th {...column.getHeaderProps()}>{column.render('Header')}</th>
-              ))}
-            </tr>
-          ))}
-        </thead>
-        <tbody {...getTableBodyProps()}>
-          {page.map((row, i) => {
-            prepareRow(row)
-            return (
-              <tr {...row.getRowProps()}>
-                {row.cells.map(cell => {
-                  return <td {...cell.getCellProps()}>{cell.render('Cell')}</td>
-                })}
-              </tr>
-            )
-          })}
-        </tbody>
-      </table>
-
-
-
-      {/* 
-        Pagination can be built however you'd like. 
-        This is just a very basic UI implementation:
-      */}
-
-
-      <div className={style["pagination"]}>
-        <button onClick={() => gotoPage(0)} disabled={!canPreviousPage}>
-          {'<<'}
-        </button>{' '}
-        <button onClick={() => previousPage()} disabled={!canPreviousPage}>
-          {'<'}
-        </button>{' '}
-        <button onClick={() => nextPage()} disabled={!canNextPage}>
-          {'>'}
-        </button>{' '}
-        <button onClick={() => gotoPage(pageCount - 1)} disabled={!canNextPage}>
-          {'>>'}
-        </button>{' '}
-        <span>
-          Page{' '}
-          <strong>
-            {pageIndex + 1} of {pageOptions.length}
-          </strong>{' '}
-        </span>
-        <span>
-          | Go to page:{' '}
-          <input
-            type="number"
-            defaultValue={pageIndex + 1}
-            onChange={e => {
-              const page = e.target.value ? Number(e.target.value) - 1 : 0
-              gotoPage(page)
-            }}
-            style={{ width: '100px' }}
-          />
-        </span>{' '}
-        <select
-          value={pageSize}
-          onChange={e => {
-            setPageSize(Number(e.target.value))
-          }}
-        >
-          {[10, 20, 30, 40, 50].map(pageSize => (
-            <option key={pageSize} value={pageSize}>
-              Show {pageSize}
-            </option>
-          ))}
-        </select>
-      </div>
-    </>)
-
-}
 
 
 
--- src/pages/Admin/Dashboard/Requests.tsx
+++ src/pages/Admin/Dashboard/Requests.tsx
@@ -1,15 +1,12 @@
 
 import React from 'react';
-import { useTable, usePagination } from 'react-table'
 import style from './styles.module.scss';
 import { gql, useMutation, useQuery } from '@apollo/client'
-import propertyQuery, { IGetAllPropertyData, IPropertyDetails } from '../../../queries/property.query';
 import { useNavigate } from 'react-router-dom';
-import ImageSlider from '../../../components/ImageSlider';
-import propertyMutation from '../../../queries/property.mutation';
 import { useLoader } from '../../../components/Loader';
 import requestsQuery, { IRequestData } from '../../../queries/requests.query';
 import requestMutation from '../../../queries/request.mutation';
+import Table from '../../../components/Table';
 
 export default function Properties() {
     const navigate = useNavigate();
@@ -82,120 +79,5 @@ export default function Properties() {
         </div>
     </>
 }
-
-
-// table fn reusabl..
-
-
-function Table({ columns, data }: { columns: any, data: any }) {
-    const {
-
-        getTableProps,
-        getTableBodyProps,
-        headerGroups,
-        prepareRow,
-        page,
-        canPreviousPage,
-        canNextPage,
-        pageOptions,
-        pageCount,
-        gotoPage,
-        nextPage,
-        previousPage,
-        setPageSize,
-        state: { pageIndex, pageSize },
-    } = useTable(
-        {
-            columns,
-            data,
-            initialState: { pageIndex: 0 },
-        },
-        usePagination
-    )
-
-    return (
-        <>
-            <table {...getTableProps()} className={style["table"]}>
-                <thead>
-                    {headerGroups.map(headerGroup => (
-                        <tr {...headerGroup.getHeaderGroupProps()}>
-                            {headerGroup.headers.map(column => (
-                                <th {...column.getHeaderProps()}>{column.render('Header')}</th>
-                            ))}
-                        </tr>
-                    ))}
-                </thead>
-                <tbody {...getTableBodyProps()}>
-                    {page.map((row, i) => {
-                        prepareRow(row)
-                        return (
-                            <tr {...row.getRowProps()}>
-                                {row.cells.map(cell => {
-                                    return <td {...cell.getCellProps()}>{cell.render('Cell')}</td>
-                                })}
-                            </tr>
-                        )
-                    })}
-                </tbody>
-            </table>
-
-
-
-            {/* 
-        Pagination can be built however you'd like. 
-        This is just a very basic UI implementation:
-      */}
-
-
-            <div className={style["pagination"]}>
-                <button onClick={() => gotoPage(0)} disabled={!canPreviousPage}>
-                    {'<<'}
-                </button>{' '}
-                <button onClick={() => previousPage()} disabled={!canPreviousPage}>
-                    {'<'}
-                </button>{' '}
-                <button onClick={() => nextPage()} disabled={!canNextPage}>
-                    {'>'}
-                </button>{' '}
-                <button onClick={() => gotoPage(pageCount - 1)} disabled={!canNextPage}>
-                    {'>>'}
-                </button>{' '}
-                <span>
-                    Page{' '}
-                    <strong>
-                        {pageIndex + 1} of {pageOptions.length}
-                    </strong>{' '}
-                </span>
-                <span>
-                    | Go to page:{' '}
-                    <input
-                        type="number"
-                        defaultValue={pageIndex + 1}
-                        onChange={e => {
-                            const page = e.target.value ? Number(e.target.value) - 1 : 0
-                            gotoPage(page)
-                        }}
-                        style={{ width: '100px' }}
-                    />
-                </span>{' '}
-                <select
-                    value={pageSize}
-                    onChange={e => {
-                        setPageSize(Number(e.target.value))
-                    }}
-                >
-                    {[10, 20, 30, 40, 50].map(pageSize => (
-                        <option key={pageSize} value={pageSize}>
-                            Show {pageSize}
-                        </option>
-                    ))}
-                </select>
-            </div>
-        </>)
-
-}
-
-
-
 
 
