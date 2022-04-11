eca5f04f5c14a010e4797de4a2c944e9029cd9f4
vaichu <vaishnavvaichu123@gmail.com>
2022-04-11 19:10:43 +0530

    fix - browser compatibility

--- src/index.tsx
+++ src/index.tsx
@@ -1,7 +1,9 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import 'react-app-polyfill/stable';
 import '../node_modules/@fortawesome/fontawesome-free/css/all.min.css'
 import './normalize.css';
+import 'element-scroll-polyfill';
 import './base.scss';
 
 import App from './App';
