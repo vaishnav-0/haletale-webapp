6a85a1da83d56f7c29b8e7f1f779f1213fa98ac2
vaichu <vaishnavvaichu123@gmail.com>
2022-04-15 19:08:47 +0530

    improvements

--- src/components/PopupDialog.tsx
+++ src/components/PopupDialog.tsx
@@ -3,10 +3,11 @@ import { Openable } from "./Openable";
 import style from './PopupDialog.module.scss';
 
 type PropsType = {
-    message: string,
+    message: string | ((confirm: () => void, reject: () => void, close: () => void) => JSX.Element),
     onConfirm: () => void,
     onReject: () => void,
-    openState: [boolean, React.Dispatch<React.SetStateAction<boolean>>]
+    openState: [boolean, React.Dispatch<React.SetStateAction<boolean>>],
+    buttonsVisible?: boolean
 }
 export default function PopupDialog(props: PropsType) {
     return <Openable open={props.openState}>
@@ -14,33 +15,44 @@ export default function PopupDialog(props: PropsType) {
             <button onClick={() => props.openState[1](false)} className={style["btn-close"]}><i className="fas fa-times"></i></button>
             <div className={style["message"]}>
                 {
-                    props.message
+                    typeof props.message === 'string' ?
+                        props.message
+                        :
+                        props.message(props.onConfirm, props.onReject, () => props.openState[1](false))
                 }
             </div>
-            <div className={style["btn-container"]}>
-                <button onClick={() => { props.onConfirm(); props.openState[1](false) }} className={style["btn-conf"]}><i className="fas fa-check"></i></button>
-                <button onClick={() => { props.onReject(); props.openState[1](false) }} className={style["btn-rej"]}><i className="fas fa-ban"></i></button>
-            </div>
+            {
+                props.buttonsVisible &&
+                <div className={style["btn-container"]}>
+                    <button onClick={() => { props.onConfirm(); props.openState[1](false) }} className={style["btn-conf"]}><i className="fas fa-check"></i></button>
+                    <button onClick={() => { props.onReject(); props.openState[1](false) }} className={style["btn-rej"]}><i className="fas fa-ban"></i></button>
+                </div>
+            }
         </div>
     </Openable>
 }
 
-export function usePopupDialog(): [JSX.Element, (state: boolean, message: string, onConfirm?: (() => void) | undefined, onReject?: (() => void) | undefined) => void] {
+export function usePopupDialog(props: Partial<Omit<PropsType, "openState">> = {}):
+    [
+        JSX.Element,
+        (state: boolean, message?: PropsType["message"], onConfirm?: (() => void) | undefined, onReject?: (() => void) | undefined) => void
+    ] {
+    const { buttonsVisible = true, message = "", onConfirm = () => { }, onReject = () => { } } = props;
     const [open, setOpen] = React.useState<boolean>(false);
     const onConfirmRef = React.useRef(() => { });
     const onRejectRef = React.useRef(() => { });
-    const messageRef = React.useRef("");
-    const expFn = React.useCallback((state: boolean, message: string, onConfirm?: () => void, onReject?: () => void) => {
+    const messageRef = React.useRef<PropsType["message"]>("");
+    const expFn = React.useCallback((state: boolean, msg?: PropsType["message"], onCnf?: () => void, onRej?: () => void) => {
         if (!state) {
             setOpen(state)
             return;
         }
-        messageRef.current = message;
-        onConfirmRef.current = onConfirm ?? (() => { });
-        onRejectRef.current = onReject ?? (() => { });
+        messageRef.current = msg ?? message;
+        onConfirmRef.current = onCnf ?? onConfirm;
+        onRejectRef.current = onRej ?? onReject;
         setOpen(state);
     }, [])
-    return [<PopupDialog openState={[open, setOpen]} message={messageRef.current} onConfirm={onConfirmRef.current} onReject={onRejectRef.current} />,
+    return [<PopupDialog buttonsVisible={buttonsVisible} openState={[open, setOpen]} message={messageRef.current} onConfirm={onConfirmRef.current} onReject={onRejectRef.current} />,
         expFn
     ]
 }
